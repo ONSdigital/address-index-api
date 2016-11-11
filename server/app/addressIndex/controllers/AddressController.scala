14e414a5b7641f98ac30c8b60a3fde1488e6ce45
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-11-11 11:07:57 +0000

    Adding Elastic and Sheild plugins. Fixing conflicts with Build. The effort behind this PR was performed by @Mironor

--- server/app/addressIndex/controllers/AddressController.scala
+++ server/app/addressIndex/controllers/AddressController.scala
@@ -1,11 +1,20 @@
 package addressIndex.controllers
 
 import javax.inject.{Inject, Singleton}
+
 import addressIndex.modules.ElasticsearchRepository
 import play.api.Logger
 import play.api.mvc.{Action, AnyContent}
+import play.api.libs.functional.syntax._
+
 import scala.concurrent.{ExecutionContext, Future}
 import com.sksamuel.elastic4s.ElasticDsl._
+import play.api.libs.json.{JsPath, Json, Writes}
+import uk.gov.ons.addressIndex.model.AddressScheme._
+import uk.gov.ons.addressIndex.model.PostcodeAddressFile
+import uk.gov.ons.addressIndex.model.db.index.PostcodeAddressFileAddress
+
+import scala.util.matching.Regex
 
 /**
   * Main API
@@ -16,7 +25,63 @@ import com.sksamuel.elastic4s.ElasticDsl._
 @Singleton
 class AddressController @Inject()(esRepo : ElasticsearchRepository)(implicit ec : ExecutionContext) extends AddressIndexController {
 
-  val logger = Logger("address-index:AddressController")
+  val logger = Logger("address-index-server:AddressController")
+
+  case class PostcodeAddressFileReplyUnit( recordIdentifier: String,
+                                           changeType: String,
+                                           proOrder: String,
+                                           uprn: String,
+                                           udprn: String,
+                                           organizationName: String,
+                                           departmentName: String,
+                                           subBuildingName: String,
+                                           buildingName: String,
+                                           buildingNumber: String,
+                                           dependentThoroughfare: String,
+                                           thoroughfare: String,
+                                           doubleDependentLocality: String,
+                                           dependentLocality: String,
+                                           postTown: String,
+                                           postcode: String,
+                                           postcodeType: String,
+                                           deliveryPointSuffix: String,
+                                           poBoxNumber: String,
+                                           startDate: String,
+                                           lastUpdateDate: String)
+
+  /**
+    * The initial data object contains more than 22 fields, hence it cannot be translated into json.
+    * That's why we need this one
+    */
+  object PostcodeAddressFileReplyUnit {
+    implicit val pafReplyUnitWrites = Json.format[PostcodeAddressFileReplyUnit]
+
+    def fromPostcodeAddressFileAddress(other: PostcodeAddressFileAddress): PostcodeAddressFileReplyUnit =
+      PostcodeAddressFileReplyUnit(
+        other.recordIdentifier,
+        other.changeType,
+        other.proOrder,
+        other.uprn,
+        other.udprn,
+        other.organizationName,
+        other.departmentName,
+        other.subBuildingName,
+        other.buildingName,
+        other.buildingNumber,
+        other.dependentThoroughfare,
+        other.thoroughfare,
+        other.doubleDependentLocality,
+        other.dependentLocality,
+        other.postTown,
+        other.postcode,
+        other.postcodeType,
+        other.deliveryPointSuffix,
+        other.poBoxNumber,
+        other.startDate,
+        other.lastUpdateDate
+      )
+  }
+
 
   /**
     * Test elastic is connected
@@ -38,11 +103,25 @@ class AddressController @Inject()(esRepo : ElasticsearchRepository)(implicit ec 
     * @return
     */
   def addressQuery(
-    format : String,
-    input  : String
+    input  : String,
+    format : String
   ) : Action[AnyContent] = Action async {  implicit req =>
-    logger info "#addressQuery called"
-    Future successful NotImplemented
+    logger info s"#addressQuery called with input $input"
+
+    val regex : Regex = "(?:[A-Za-z]\\d ?\\d[A-Za-z]{2})|(?:[A-Za-z][A-Za-z\\d]\\d ?\\d[A-Za-z]{2})|(?:[A-Za-z]{2}\\d{2} ?\\d[A-Za-z]{2})|(?:[A-Za-z]\\d[A-Za-z] ?\\d[A-Za-z]{2})|(?:[A-Za-z]{2}\\d[A-Za-z] ?\\d[A-Za-z]{2})".r
+    val postcode : String = regex.findFirstIn(input).getOrElse("Not recognised")
+    val buildingNumber : Int = input.substring(0, 1).toInt
+
+    logger info s"#addressQuery parsed: postcode: $postcode , buildingNumber: $buildingNumber"
+
+    Future.fromTry(format.toAddressScheme()).flatMap {
+      case PostcodeAddressFile() => esRepo.queryAddress(buildingNumber, postcode).map {
+        addresses => logger info "#addressQuery got a response from es";Ok {
+          Json.toJson(addresses.map(PostcodeAddressFileReplyUnit.fromPostcodeAddressFileAddress))
+        }
+      }
+      case _ => Future.successful(Ok("Wrong format"))
+    }
   }
 
   /**
@@ -53,16 +132,20 @@ class AddressController @Inject()(esRepo : ElasticsearchRepository)(implicit ec 
     * @return
     */
   def uprnQuery(
-    uprn   : String,
-    format : String
+                 uprn   : String,
+                 format : String
   ) : Action[AnyContent] = Action async { implicit req =>
     logger info "#uprnQuery called"
-    //dummy data
-    val map : Map[(String, String), String] = Map(
-      "paf" -> "XXX" -> "SomeAddress"
-    )
-    Future successful Ok(
-      map getOrElse((format, uprn), s"UPRN '$uprn' for format '$format' not Found")
-    )
+
+
+    Future.fromTry(format.toAddressScheme()).flatMap {
+      case PostcodeAddressFile() => esRepo.queryUprn(uprn).map {
+        addresses => Ok {
+          Json.toJson(addresses.map(PostcodeAddressFileReplyUnit.fromPostcodeAddressFileAddress))
+        }
+      }
+      case _ => Future.successful(Ok("Wrong format"))
+    }
   }
+
 }
--- server/app/addressIndex/modules/AddressIndexRepository.scala
+++ server/app/addressIndex/modules/AddressIndexRepository.scala
@@ -3,13 +3,17 @@ package addressIndex.modules
 import javax.inject.{Inject, Singleton}
 
 import com.sksamuel.elastic4s._
+import com.sksamuel.elastic4s.ElasticDsl._
 import org.elasticsearch.common.settings._
 import com.google.inject.ImplementedBy
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse
+import org.elasticsearch.plugins.Plugin
+import org.elasticsearch.shield.ShieldPlugin
 import play.api.Logger
-import uk.gov.ons.addressIndex.model.db.index.{PostcodeAddressFileIndex, PostcodeIndex}
+import uk.gov.ons.addressIndex.model.db.index.{PostcodeAddressFileAddress, PostcodeIndex}
 import uk.gov.ons.addressIndex.model.db.ElasticIndexSugar
+
 import scala.concurrent.{ExecutionContext, Future}
 
 @ImplementedBy(classOf[AddressIndexRepository])
@@ -32,6 +36,9 @@ trait ElasticsearchRepository extends ElasticIndexSugar {
     * @return
     */
   def deleteAll() : Future[Seq[_]]
+
+  def queryUprn(uprn: String) : Future[Seq[PostcodeAddressFileAddress]]
+  def queryAddress(buildingNumber : Int, postcode : String) : Future[Seq[PostcodeAddressFileAddress]]
 }
 
 @Singleton
@@ -45,19 +52,24 @@ class AddressIndexRepository @Inject()(conf : AddressIndexConfigModule)(implicit
     */
   val client : ElasticClient = {
     logger info s"attempting to connect to elasticsearch uri: ${esConf.uri} cluster: ${esConf.cluster}"
-    val esClientSettings = Settings.settingsBuilder.put(
-      "cluster.name" -> esConf.cluster,
-      "client" -> esConf.uri
-    ).build
+
+    val esClientSettings = Settings.settingsBuilder
+      .put("cluster.name", esConf.cluster)
+      .put("shield.transport.ssl", esConf.shieldSsl)
+      .put("request.headers.X-Found-Cluster", esConf.cluster)
+      .put("shield.user", esConf.shieldUser)
+      .build()
 
     if(esConf.local) {
       val client = ElasticClient local esClientSettings
       logger info "local connection to elasticsearch established"
       client
     } else {
+      val plugins: Class[_ <: Plugin] = classOf[ShieldPlugin]
       val client = ElasticClient.transport(
         settings = esClientSettings,
-        uri = ElasticsearchClientUri(esConf.uri)
+        uri = ElasticsearchClientUri(esConf.uri),
+        plugins = plugins
       )
       logger info "remote connection to elasticsearch established"
       client
@@ -66,15 +78,36 @@ class AddressIndexRepository @Inject()(conf : AddressIndexConfigModule)(implicit
 
   def createAll() : Future[Seq[CreateIndexResponse]] = {
     createIndex(
-      PostcodeAddressFileIndex,
+      PostcodeAddressFileAddress,
       PostcodeIndex
     )(client)
   }
 
   def deleteAll() : Future[Seq[DeleteIndexResponse]] = {
     deleteIndex(
-      PostcodeAddressFileIndex,
+      PostcodeAddressFileAddress,
       PostcodeIndex
     )(client)
   }
+
+  def queryUprn(uprn: String): Future[Seq[PostcodeAddressFileAddress]] = client.execute{
+    search in "paf/address" query { termQuery("uprn", uprn) }
+  }.map(_.as[PostcodeAddressFileAddress])
+
+  def queryAddress(buildingNumber : Int, postcode : String) : Future[Seq[PostcodeAddressFileAddress]] = client.execute {
+    search in "paf/address" query {
+      bool(
+        must(
+          matchQuery(
+            field = "buildingNumber",
+            value = buildingNumber
+          ),
+          matchQuery(
+            field = "postcode",
+            value = postcode
+          )
+        )
+      )
+    }
+  }.map(_.as[PostcodeAddressFileAddress])
 }
