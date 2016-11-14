a5800296c1c74c3a965e79930c3e1827895188a5
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-11-14 11:57:43 +0000

    fixing tests

--- parsers/src/test/scala/uk/gov/ons/addressIndex/parsers/StringUtilsTest.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/parsers/StringUtilsTest.scala
@@ -13,37 +13,37 @@ class StringUtilsTest extends FlatSpec with Matchers {
 
   it should "return false for `hello` when invoking `allDigits`" in {
     val expected = false
-    val actual = !"hello".allDigits[Boolean](identity)
+    val actual = "hello".allDigits[Boolean](identity)
     actual shouldBe expected
   }
 
   it should "return false for `hello123` when invoking `allDigits`" in {
     val expected = false
-    val actual = !"hello123".allDigits[Boolean](identity)
+    val actual = "hello123".allDigits[Boolean](identity)
     actual shouldBe expected
   }
 
   it should "return true for `hello123` when invoking `containsDigits`" in {
     val expected = true
-    val actual = !"hello123".containsDigits[Boolean](identity)
+    val actual = "hello123".containsDigits[Boolean](identity)
     actual shouldBe expected
   }
 
   it should "return false for `hello` when invoking `containsDigits`" in {
     val expected = false
-    val actual = !"hello".containsDigits[Boolean](identity)
+    val actual = "hello".containsDigits[Boolean](identity)
     actual shouldBe expected
   }
 
   it should "return true for `hello` when invoking `containsVowels`" in {
     val expected = true
-    val actual = !"hello".containsVowels[Boolean](identity)
+    val actual = "hello".containsVowels[Boolean](identity)
     actual shouldBe expected
   }
 
   it should "return false for `hll` when invoking `containsVowels`" in {
     val expected = false
-    val actual = !"hll".containsVowels[Boolean](identity)
+    val actual = "hll".containsVowels[Boolean](identity)
     actual shouldBe expected
   }
 }
