7d9b485417afcea940397110a07ae2004dbcbed0
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-11-15 09:33:03 +0000

    fixing tests

--- model/src/test/scala/uk/gov/ons/addressIndex/model/config/AddressIndexConfigTest.scala
+++ model/src/test/scala/uk/gov/ons/addressIndex/model/config/AddressIndexConfigTest.scala
@@ -7,14 +7,12 @@ class AddressIndexConfigTest extends FlatSpec with Matchers {
   it should "produce the expected default config" in {
     val expected = AddressIndexConfigTest.default
     val actual = AddressIndexConfig.default
-
     actual shouldBe expected
   }
 }
 
 object AddressIndexConfigTest {
   val default = AddressIndexConfig(
-    runMode = "dev",
     elasticSearch = ElasticSearchConfigTest.default
   )
 }
@@ -24,7 +22,6 @@ class ElasticSearchConfigTest extends FlatSpec with Matchers {
   it should "produce the expected default config" in {
     val expected = ElasticSearchConfigTest.default
     val actual = ElasticSearchConfig.default
-
     actual shouldBe expected
   }
 }
