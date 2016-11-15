a1f16ef8687e9ed5ffe26e9cc589205c9d7d85db
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-11-15 09:27:09 +0000

    removing used example form

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/views/forms/Search.html.scala
+++
@@ -1,7 +0,0 @@
-@(id : String, action : String, method : String = "GET", search: String, reset: String)
-
-<form id="@id" action="@action" method="@method">
-  <input name="search" />
-  <button type="submit">@search</button>
-  <button type="reset">@reset</button>
-</form>
