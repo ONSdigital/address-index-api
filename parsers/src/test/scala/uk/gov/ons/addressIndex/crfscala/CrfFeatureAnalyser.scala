1bc4ec6042c258b3081daf0ff3af835c6910b223
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-11-17 12:25:57 +0000

    adding tests for CrfScala lib

--- parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureAnalyser.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureAnalyser.scala
@@ -2,7 +2,3 @@ package uk.gov.ons.addressIndex.crfscala
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class CrfFeatureAnalyser extends FlatSpec with Matchers {
-
-
-}
--- parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureTest.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureTest.scala
@@ -1,7 +1,99 @@
 package uk.gov.ons.addressIndex.crfscala
 
 import org.scalatest.{FlatSpec, Matchers}
+import uk.gov.ons.addressIndex.crfscala.CrfScala.{CrfFeature, CrfFeatureAnalyser}
+import third_party.org.chokkan.crfsuite.Attribute
+import scala.util.Try
 
-class CrfFeatureTest extends FlatSpec with Matchers {
+class CrfFeatureTest extends FlatSpec with Matchers  {
 
+  object TestInstanceBoolean {
+    type tType = Boolean
+    val analyser = CrfFeatureAnalyser[tType](_ => true)
+    val name = "name"
+    val feature = CrfFeatureTestImpl[tType](name)(analyser)
+  }
+
+  it should "return the same type as the type which is returned from its analyser for input `arbitrary`" in {
+    val input = "arbitrary"
+    val actual = TestInstanceBoolean.feature.analyse(input)
+    assert(actual.isInstanceOf[TestInstanceBoolean.tType])
+  }
+
+  it should "return the expected attribute for type `Boolean` for input `arbitrary`" in {
+    val input = "arbitrary"
+    val actual = TestInstanceBoolean.feature.toAttribute(input)
+    val attributesValue : Double = if(TestInstanceBoolean.feature.analyse(input)) 1d else 0d
+    val expected = new Attribute(TestInstanceBoolean.feature.name, attributesValue)
+    actual.getAttr shouldBe expected.getAttr
+    actual.getValue shouldBe expected.getValue
+  }
+
+
+  object TestInstanceString {
+    type tType = String
+    val analyser = CrfFeatureAnalyser[tType](identity)
+    val name = "name"
+    val feature = CrfFeatureTestImpl[tType](name)(analyser)
+  }
+
+  it should "return the expected attribute for type `String` for input `arbitrary`" in {
+    val input = "arbitrary"
+    val actual = TestInstanceString.feature.toAttribute(input)
+    val attribute : String = s"${TestInstanceString.feature.name}=$input"
+    val expected = new Attribute(attribute)
+    actual.getAttr shouldBe expected.getAttr
+  }
+
+
+  object TestInstanceInt {
+    type tType = Int
+    val output : tType = 0
+    val analyser = CrfFeatureAnalyser[tType](_ => output)
+    val name = "name"
+    val feature = CrfFeatureTestImpl[tType](name)(analyser)
+  }
+
+  it should "return the expected attribute for type `Int` for input `arbitrary`" in {
+    val input = "arbitrary"
+    val actual = TestInstanceInt.feature.toAttribute(input)
+    val expected =  new Attribute(TestInstanceInt.feature.name, Int int2double TestInstanceInt.output)
+    actual.getAttr shouldBe expected.getAttr
+  }
+
+
+  object TestInstanceDouble {
+    type tType = Double
+    val output : tType = 0
+    val analyser = CrfFeatureAnalyser[tType](_ => output)
+    val name = "name"
+    val feature = CrfFeatureTestImpl[tType](name)(analyser)
+  }
+
+  it should "return the expected attribute for type `Double` for input `arbitrary`" in {
+    val input = "arbitrary"
+    val actual = TestInstanceDouble.feature.toAttribute(input)
+    val expected =  new Attribute(TestInstanceInt.feature.name, TestInstanceInt.output)
+    actual.getAttr shouldBe expected.getAttr
+  }
+
+
+  object TestInstanceArbType {
+    case class ArbType()
+    type tType = ArbType
+    val output : tType = ArbType()
+    val analyser = CrfFeatureAnalyser[tType](_ => output)
+    val name = "name"
+    val feature = CrfFeatureTestImpl[tType](name)(analyser)
+  }
+
+  it should "throw an `UnsupportedOperationException` for any type other than `Boolean`, `String`, `Int`, `Double` for input `arbitrary`" in {
+    val input = "arbitrary"
+    Try[Attribute](TestInstanceArbType.feature.toAttribute(input)) recover {
+      case _ : UnsupportedOperationException => assert(true)
+      case _ => assert(false)
+    }
+  }
 }
+
+case class CrfFeatureTestImpl[T](override val name : String)(override val analyser : CrfFeatureAnalyser[T]) extends CrfFeature[T]
