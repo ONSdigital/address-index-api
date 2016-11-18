d399915741878def1b2da20492321e12b3a63e05
RichardSmithONS <richard.m.smith@ons.gsi.gov.uk>
2016-11-18 15:25:57 +0000

    another interim checkin, still quite a bit to do

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/ApplicationHome.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/ApplicationHome.scala
@@ -5,12 +5,16 @@ import play.Logger
 import play.api.mvc.{Controller, _}
 import play.api.mvc.Action
 import scala.concurrent.ExecutionContext
+import scala.language.implicitConversions
+import play.api.i18n.{I18nSupport, Messages, MessagesApi, Lang}
 
 @Singleton
-class ApplicationHome @Inject()(implicit ec : ExecutionContext) extends Controller {
+class ApplicationHome @Inject()(implicit ec : ExecutionContext, val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def indexPage() : Action[AnyContent] = Action { implicit req =>
     Logger.info("Rendering Index page")
-    Ok(uk.gov.ons.addressIndex.demoui.views.html.index())
+    req.getQueryString("lang") match{
+      case Some(lang) =>  messagesApi.setLang(Ok(uk.gov.ons.addressIndex.demoui.views.html.index()(messagesApi,Lang(lang))),Lang(lang))
+      case None => messagesApi.setLang(Ok(uk.gov.ons.addressIndex.demoui.views.html.index()(messagesApi,Lang("en"))),Lang("en"))
   }
-}
+}}
---
+++ demo-ui/conf/messages.cy
@@ -0,0 +1,3 @@
+home.servicetext=Gwasanaeth Paru Cyfeiriad
+home.headertext=Mae''r gwasanaeth hwn yn eich galluogi i chwilio am gyfeiriad unigol, yn cyd-fynd swp o gyfeiriadau o ffeil a ddewiswyd neu i echdynnu gyfeiriad rhestri o''r gronfa ddata.
+single.pleasesupply="Rhowch gyfeiriad"
