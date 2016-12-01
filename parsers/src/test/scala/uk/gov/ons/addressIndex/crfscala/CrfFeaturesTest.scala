8f6bcc77ad63c2a848f82b6687b673ffa9777c2e
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-01 14:51:45 +0000

    fixing test names

--- parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeaturesTest.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeaturesTest.scala
@@ -4,21 +4,21 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CrfFeaturesTest extends FlatSpec with Matchers {
 
-  it should "return all features" in {
+  it should "return all feature analysers" in {
     val expected = Seq(CrfFeatureTestImpl[Boolean]("name")(str => true))
     val test = CrfFeaturesImpl(expected)
     val actual = test.all
     actual should contain theSameElementsAs expected
   }
 
-  it should "return the expected amount of `Item`s for input `arbitrary` (String, Boolean, Double, Int)" in {
+  it should "return the expected `CrfJniInput` for a combination of feature analysers (String, Boolean, Double, Int - (all supported types))" in {
     val input = "arbitrary"
     val feature1 = CrfFeatureTestImpl[String]("name1")(str => str)
     val feature2 = CrfFeatureTestImpl[Boolean]("name2")(str => true)
     val feature3 = CrfFeatureTestImpl[Double]("name3")(str => 0d)
     val feature4 = CrfFeatureTestImpl[Int]("name4")(str => 0)
     val test = CrfFeaturesImpl(Seq(feature1, feature2, feature3, feature4))
-    val actual = test.toCrfJniInput(input)
+    val actual = test toCrfJniInput input
     val expected = s"\t${feature1.name}\\:$input:1.0\t${feature2.name}:1.0\t${feature3.name}:0.0\t${feature4.name}:0.0\n"
     actual shouldBe expected
   }
