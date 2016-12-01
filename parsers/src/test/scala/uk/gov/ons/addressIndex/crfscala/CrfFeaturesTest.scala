0ba10f55470edb41e706b4db2a312954d6c8c818
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-01 12:28:07 +0000

    removing redundant tests. we are no longer uing JCrfSuite. We are constructing strings are interfacing via JNI instead

--- parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeaturesTest.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeaturesTest.scala
@@ -5,50 +5,11 @@ import uk.gov.ons.addressIndex.crfscala.CrfScala.{CrfFeature, CrfFeatures}
 
 class CrfFeaturesTest extends FlatSpec with Matchers {
 
-  ignore should "return all features" in {
+  it should "return all features" in {
     val expected = Seq(CrfFeatureTestImpl[Boolean]("name")(str => true))
     val test = CrfFeaturesImpl(expected)
     val actual = test.all
     actual should contain theSameElementsAs expected
-  }
-
-  ignore should "return the expected amount of `Item`s for input `arbitrary` (Boolean)" in {
-    val input = "arbitrary"
-    val feature = CrfFeatureTestImpl[Boolean]("name")(str => true)
-//    val expected = new Item()
-//    expected.add(feature toCrfJniInput input)
-    val test = CrfFeaturesImpl(Seq(feature))
-//    val actual = test toItem input
-//    actual.size shouldBe expected.size
-  }
-
-  ignore should "return the expected amount of `Item`s for input `arbitrary` (Int)" in {
-    val input = "arbitrary"
-    val feature = CrfFeatureTestImpl[Int]("name")(str => 0)
-//    expected.add(feature toCrfJniInput input)
-    val test = CrfFeaturesImpl(Seq(feature))
-//    val actual = test toItem input
-//    actual.size shouldBe expected.size
-  }
-
-  ignore should "return the expected amount of `Item`s for input `arbitrary` (Double)" in {
-    val input = "arbitrary"
-    val feature = CrfFeatureTestImpl[Double]("name")(str => 0d)
-//    val expected = new Item()
-//    expected.add(feature toCrfJniInput input)
-    val test = CrfFeaturesImpl(Seq(feature))
-//    val actual = test toItem input
-//    actual.size shouldBe expected.size
-  }
-
-  ignore should "return the expected amount of `Item`s for input `arbitrary` (String)" in {
-    val input = "arbitrary"
-    val feature = CrfFeatureTestImpl[String]("name")(str => "")
-//    val expected = new Item()
-//    expected.add(feature toCrfJniInput input)
-    val test = CrfFeaturesImpl(Seq(feature))
-//    val actual = test toItem input
-//    actual.size shouldBe expected.size
   }
 
   ignore should "return the expected amount of `Item`s for input `arbitrary` (String, Boolean, Double, Int)" in {
