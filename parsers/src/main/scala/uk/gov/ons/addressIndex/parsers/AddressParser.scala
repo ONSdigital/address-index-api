0a4e7274b2d931439ea9fbe85dbd372fc281551a
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-07 10:51:01 +0000

    cleaning crfparser impl

--- parsers/src/main/scala/uk/gov/ons/addressIndex/parsers/AddressParser.scala
+++ parsers/src/main/scala/uk/gov/ons/addressIndex/parsers/AddressParser.scala
@@ -3,36 +3,15 @@ package uk.gov.ons.addressIndex.parsers
 import uk.gov.ons.addressIndex.crfscala.CrfAggregateFeatureAnalyser.CrfAggregateFeatureAnalyser
 import uk.gov.ons.addressIndex.crfscala.CrfFeatureAnalyser.CrfFeatureAnalyser
 import uk.gov.ons.addressIndex.crfscala.{CrfAggregateFeature, CrfFeature, CrfFeatures, CrfParser}
-import uk.gov.ons.addressIndex.crfscala.CrfScala._
 
 //TODO scaladoc
 /**
   * AddressParser
   */
-object AddressParser extends CrfParser {
-  //can remove
-  def parse(i: Input, fa: Features, tokenable: CrfTokenable): CrfJniInput = {
-    super.parse(i, fa, tokenable)
-  }
-}
+object AddressParser extends CrfParser
 
-/**
-  * Feature collection
-  *
-  * scala wrapper on third_party.org.chokkan.crfsuite.Item
-  *
-  * @param features the features of this feature collection
-  */
 case class Features(override val features : Feature[_]*)(override val aggregateFeatures: FeatureAggregate[_]*) extends CrfFeatures
 
-/**
-  * scala wrapper on third_party.org.chokkan.crfsuite.Attribute
-  *
-  * @param name the feature's key which is referenced in them jcrfsuite model
-  *
-  * @param analyser feature analyser
-  *
-  * @tparam T the return type of this analyser; used for the conversion to an Item
-  */
 case class Feature[T](override val name: String)(override val analyser: CrfFeatureAnalyser[T]) extends CrfFeature[T]
+
 case class FeatureAggregate[T](override val name: String)(override val analyser: CrfAggregateFeatureAnalyser[T]) extends CrfAggregateFeature[T]
