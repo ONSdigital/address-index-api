c85ffbbf83ffdbc70870d2212997bcc6e138973b
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-09 15:47:26 +0000

    addressing pr feedback

--- parsers/src/main/scala/uk/gov/ons/addressIndex/crfscala/CrfFeature.scala
+++ parsers/src/main/scala/uk/gov/ons/addressIndex/crfscala/CrfFeature.scala
@@ -24,7 +24,7 @@ trait CrfFeature[T] {
     * @param input
     * @return apply the analyser to i
     */
-  def analyse(input : Input): T = analyser apply input
+  def analyse(input: Input): T = analyser apply input
 
   //TODO scaladoc
   /**
