b940c1e0b64d8516a6dbd1a29c1c02b67e5d0bf4
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-12 11:45:09 +0000

    fixing test to use shared typeclass

--- parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureTest.scala
+++ parsers/src/test/scala/uk/gov/ons/addressIndex/crfscala/CrfFeatureTest.scala
@@ -2,115 +2,96 @@ package uk.gov.ons.addressIndex.crfscala
 
 import org.scalatest.{FlatSpec, Matchers}
 import uk.gov.ons.addressIndex.crfscala.CrfFeatureAnalyser.CrfFeatureAnalyser
-import scala.util.Try
 
 class CrfFeatureTest extends FlatSpec with Matchers  {
 
-  object TestInstanceBoolean {
-    type tType = Boolean
-    val analyser = CrfFeatureAnalyser[tType](_ => true)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
-  }
-
   it should "return the same type as the type which is returned from its analyser for input `arbitrary`" in {
     val input = "arbitrary"
-    val actual = TestInstanceBoolean.feature.analyse(input)
+    val actual = Instances.TestInstanceBoolean.feature.analyse(input)
     actual shouldBe a [java.lang.Boolean]
   }
 
   it should "return the expected `CrfJniInput` for type `Boolean` for input `arbitrary`" in {
     val input = "arbitrary"
-    val actual = TestInstanceBoolean.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceBoolean.name}:1.0"
+    val actual = Instances.TestInstanceBoolean.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceBoolean.name}:1.0"
     expected shouldBe actual
-  }
-
-  object TestInstanceStringA {
-    type tType = String
-    val analyser = CrfFeatureAnalyser[tType](identity)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "return the expected `CrfJniInput` for type `String` for input `arbitrary`" in {
     val input = "arbitrary"
-    val actual = TestInstanceStringA.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceStringA.name}\\:$input:1.0"
+    val actual = Instances.TestInstanceStringA.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceStringA.name}\\:$input:1.0"
     expected shouldBe actual
-  }
-
-  object TestInstanceStringB {
-    type tType = String
-    val analyser = CrfFeatureAnalyser[tType](identity)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "return the expected `CrfJniInput` for type `String` for input `arbitrary:arbitrary`" in {
     val input = "arbitrary:arbitrary"
-    val actual = TestInstanceStringB.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceStringB.name}\\:${input.replace(":", "\\:")}:1.0"
+    val actual = Instances.TestInstanceStringB.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceStringB.name}\\:${input.replace(":", "\\:")}:1.0"
     expected shouldBe actual
-  }
-
-  object TestInstanceInt {
-    type tType = Int
-    val output: tType = 0
-    val analyser = CrfFeatureAnalyser[tType](_ => output)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "return the expected `CrfJniInput` for type `Int` for input `arbitrary`" in {
     val input = "arbitrary"
-    val actual = TestInstanceInt.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceInt.name}:0.0"
+    val actual = Instances.TestInstanceInt.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceInt.name}:0.0"
     expected shouldBe actual
-  }
-
-  object TestInstanceDouble {
-    type tType = Double
-    val output: tType = 0d
-    val analyser = CrfFeatureAnalyser[tType](_ => output)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "return the expected `CrfJniInput` for type `Double` for input `arbitrary`" in {
     val input = "arbitrary"
-    val actual = TestInstanceDouble.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceDouble.name}:0.0"
+    val actual = Instances.TestInstanceDouble.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceDouble.name}:0.0"
     actual shouldBe expected
-  }
-
-  object TestInstanceQualifyName {
-    type tType = Double
-    val output: tType = 0d
-    val analyser = CrfFeatureAnalyser[tType](_ => output)
-    val name = "name:WithSomethingToQualify"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "qualify any feature analyser's name with a `:` in it across all supported return types" in {
     val input = "arbitrary"
-    val actual = TestInstanceQualifyName.feature.toCrfJniInput(input)
-    val expected = s"\t${TestInstanceQualifyName.name.replace(":", "\\:")}:0.0"
+    val actual = Instances.TestInstanceQualifyName.feature.toCrfJniInput(input)
+    val expected = s"\t${Instances.TestInstanceQualifyName.name.replace(":", "\\:")}:0.0"
     actual shouldBe expected
-  }
-
-  object TestInstanceArbType {
-    case class ArbType()
-    type tType = ArbType
-    val output: tType = ArbType()
-    val analyser = CrfFeatureAnalyser[tType](_ => output)
-    val name = "name"
-    val feature = CrfFeatureTestImpl[tType](name)(analyser)
   }
 
   it should "throw an `UnsupportedOperationException` for any type other than `Boolean`, `String`, `Int`, `Double` for input `arbitrary`" in {
     val input = "arbitrary"
-    an [UnsupportedOperationException] should be thrownBy TestInstanceArbType.feature.toCrfJniInput(input)
+    an [UnsupportedOperationException] should be thrownBy Instances.TestInstanceArbType.feature.toCrfJniInput(input)
+  }
+
+  trait TestInstance[T] {
+    def output: T
+    def analyser = CrfFeatureAnalyser[T](_ => output)
+    def name = "name"
+    def feature = CrfFeatureTestImpl[T](name)(analyser)
+  }
+
+  object Instances {
+    def apply[T]()(implicit ev: TestInstance[T]): TestInstance[T] = ev
+
+    case class ArbType()
+
+    implicit object TestInstanceArbType extends TestInstance[ArbType] {
+      def output = ArbType()
+    }
+    implicit object TestInstanceBoolean extends TestInstance[Boolean] {
+      def output = true
+    }
+    implicit object TestInstanceInt extends TestInstance[Int] {
+      def output = 0
+    }
+    implicit object TestInstanceStringA extends TestInstance[String] {
+      def output = "arbitrary"
+    }
+    implicit object TestInstanceStringB extends TestInstance[String] {
+      def output = "arbitrary:arbitrary"
+    }
+    implicit object TestInstanceDouble extends TestInstance[Double] {
+      def output = 0d
+    }
+    implicit object TestInstanceQualifyName extends TestInstance[Double] {
+      def output = 0d
+      override def name = "name:WithSomethingToQualify"
+    }
   }
 }
 
