69ffe54cc20dc83245c84282ec1d4bda44626f72
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2016-12-14 15:54:18 +0000

    adding mixin on abstract layer

--- server/app/uk/gov/ons/addressIndex/server/controllers/AddressIndexController.scala
+++ server/app/uk/gov/ons/addressIndex/server/controllers/AddressIndexController.scala
@@ -1,4 +1,7 @@
 package uk.gov.ons.addressIndex.server.controllers
 
+import uk.gov.ons.addressIndex.server.modules.AddressIndexCannedResponse
+
 abstract class AddressIndexController
   extends PlayHelperController
+  with AddressIndexCannedResponse
