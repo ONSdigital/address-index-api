64a61ca8336f7ddf82e50c45708c86102d2849a1
Aliaksandr.Bedrytski <aliaksandr.bedrytski@valtech.co.uk>
2016-12-15 09:15:42 +0000

    Addresses feedback

--- model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/AddressResponse.scala
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/AddressResponse.scala
@@ -246,14 +246,14 @@ object AddressResponseAddress {
     val saoRightRangeExists = nag.saoEndNumber.nonEmpty || nag.saoEndSuffix.nonEmpty
     val saoHyphen = if (saoLeftRangeExists && saoRightRangeExists) "-" else ""
     val saoNumbers = Seq(nag.saoStartNumber, nag.saoStartSuffix, saoHyphen, nag.saoEndNumber, nag.saoEndSuffix)
-      .map(_.trim).mkString("")
+      .map(_.trim).mkString
     val sao = if (nag.saoText == nag.organisation) saoNumbers else s"$saoNumbers ${nag.saoText}"
 
     val paoLeftRangeExists = nag.paoStartNumber.nonEmpty || nag.paoStartSuffix.nonEmpty
     val paoRightRangeExists = nag.paoEndNumber.nonEmpty || nag.paoEndSuffix.nonEmpty
     val paoHyphen = if (paoLeftRangeExists && paoRightRangeExists) "-" else ""
     val paoNumbers = Seq(nag.paoStartNumber, nag.paoStartSuffix, paoHyphen, nag.paoEndNumber, nag.paoEndSuffix)
-      .map(_.trim).mkString("")
+      .map(_.trim).mkString
     val pao = if (nag.paoText == nag.organisation) paoNumbers else s"${nag.paoText} $paoNumbers"
 
     Seq(nag.organisation, sao, pao, nag.streetDescriptor, nag.locality,
