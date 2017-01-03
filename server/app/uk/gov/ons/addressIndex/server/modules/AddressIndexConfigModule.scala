85b66226f13964be9332f2b642db67558c276529
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-03 09:25:40 +0000

    cleaning up style

--- server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexConfigModule.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexConfigModule.scala
@@ -12,6 +12,6 @@ import scala.util.Try
 class AddressIndexConfigModule() {
   //TODO should we be defaulting here?
   //TODO use the `tryConfig` for errors later on?
-  private val tryConfig : Try[AddressIndexConfig] = loadConfig[AddressIndexConfig]("addressIndex")
-  val config : AddressIndexConfig = tryConfig getOrElse AddressIndexConfig.default
+  private val tryConfig: Try[AddressIndexConfig] = loadConfig[AddressIndexConfig]("addressIndex")
+  val config: AddressIndexConfig = tryConfig getOrElse AddressIndexConfig.default
 }
