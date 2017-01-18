bda81a2151a780feccb4b43f96ec98015ea62b77
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-18 15:43:54 +0000

    changing elastic health route name

--- server/app/uk/gov/ons/addressIndex/server/controllers/ElasticController.scala
+++ server/app/uk/gov/ons/addressIndex/server/controllers/ElasticController.scala
@@ -4,15 +4,18 @@ import com.sksamuel.elastic4s.ElasticDsl._
 import play.api.mvc.{Action, AnyContent}
 import javax.inject.{Inject, Singleton}
 import uk.gov.ons.addressIndex.server.modules.ElasticsearchRepository
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class ElasticController @Inject()(esRepo: ElasticsearchRepository) extends AddressIndexController {
+class ElasticController @Inject()(esRepo: ElasticsearchRepository)(implicit ec: ExecutionContext)
+  extends AddressIndexController {
+
   /**
     * Test elastic is connected
     *
     * @return
     */
-  def elasticTest(): Action[AnyContent] = Action async { implicit req =>
+  def clusterHealth(): Action[AnyContent] = Action async { implicit req =>
     esRepo.client execute {
       get cluster health
     } map { resp =>
