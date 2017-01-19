176e4f4dbab8628ad0f9b6057662fb61666a8526
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-19 12:00:05 +0000

    correct hybrid response

--- server/app/uk/gov/ons/addressIndex/server/controllers/AddressController.scala
+++ server/app/uk/gov/ons/addressIndex/server/controllers/AddressController.scala
@@ -1,52 +1,64 @@
 package uk.gov.ons.addressIndex.server.controllers
 
+import java.util
 import javax.inject.{Inject, Singleton}
+
 import com.sksamuel.elastic4s.{HitAs, RichSearchHit}
 import uk.gov.ons.addressIndex.server.modules.{AddressIndexActions, AddressParserModule, ElasticSearchRepository}
 import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
+
 import scala.concurrent.ExecutionContext
 import uk.gov.ons.addressIndex.server.modules.AddressIndexConfigModule
 import uk.gov.ons.addressIndex.parsers.Implicits._
 import uk.gov.ons.addressIndex.server.modules.Model.Pagination
+
 import scala.util.Try
 import uk.gov.ons.addressIndex.model.AddressScheme._
 import uk.gov.ons.addressIndex.model.db.index.HybridIndex
-import uk.gov.ons.addressIndex.server.controllers.Model.HybridResponses
+import uk.gov.ons.addressIndex.server.controllers.Model.HybridResponse
+//import uk.gov.ons.addressIndex.server.controllers.Model.HybridResponses
 
 object Model {
 
 
-  case class HybridResponses(documents: Seq[HybridResponse])
-
-  implicit object HybridResponses extends HitAs[HybridResponses] {
-
-//    implicit lazy val fmt = Json.format[HybridResponses]
-
-    override def as(hit: RichSearchHit): HybridResponses = {
-      HybridResponses(
-        documents = hit.innerHits.map(_._2.asInstanceOf[HybridResponse]).toSeq
-      )
-    }
-  }
+//  case class HybridResponses(documents: Seq[HybridResponse])
+//
+//  implicit object HybridResponses extends HitAs[HybridResponses] {
+//
+////    implicit lazy val fmt = Json.format[HybridResponses]
+//
+//    override def as(hit: RichSearchHit): HybridResponses = {
+//      HybridResponses(
+//        documents = hit.sourceAsMap
+//      )
+//    }
+//  }
 
   case class HybridResponse(
     uprn: String,
-    lpi: Option[Map[String, String]],
-    paf: Option[Map[String, String]]
+    lpi: Option[Seq[Map[String, String]]],
+    paf: Option[Seq[Map[String, String]]]
   )
 
   implicit object HybridResponse extends HitAs[HybridResponse] {
+    import scala.collection.JavaConverters._
 
-//    implicit lazy val fmt = Json.format[HybridResponse]
+    implicit lazy val fmt = Json.format[HybridResponse]
 
     override def as(hit: RichSearchHit): HybridResponse = {
       val map = hit.sourceAsMap
+
+      def getSeqMap(fieldName: String): Seq[Map[String, String]] = {
+        val x = map(fieldName).asInstanceOf[util.ArrayList[java.util.HashMap[String, String]]].asScala
+        x.map(_.asScala.toMap)
+      }
+
       HybridResponse(
         uprn = map(HybridIndex.Fields.uprn).toString,
-        lpi = None,
-        paf = None
+        lpi = Some(getSeqMap(HybridIndex.Fields.lpi)),
+        paf = Some(getSeqMap(HybridIndex.Fields.paf))
       )
     }
   }
@@ -142,10 +154,15 @@ class AddressController @Inject()(
           ),
           format = format flatMap(_.stringToScheme)
         ) map { r =>
-          println(r.toString)
+//          println("r.toString" + r.toString)
+//          println("r.hits.toString")
+//          pprint.pprintln(r.hits.map(_.sourceAsMap))
+
+            r.as[HybridResponse]
+
 
 //          jsonOk(r.as[HybridResponses].toSeq)
-          jsonOk("done")
+          jsonOk(r.as[HybridResponse])
         }
       } getOrElse futureJsonBadRequest(EmptySearch)
     }
