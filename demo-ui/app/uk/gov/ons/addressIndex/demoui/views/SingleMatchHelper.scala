759deac3cf79df5e1ec10b4c5aa41b0c2685ccac
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-23 11:48:21 +0000

    adding SingleMatchHelper so views dont contain that much scala

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/views/SingleMatchHelper.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/views/SingleMatchHelper.scala
@@ -1,8 +1,31 @@
 package uk.gov.ons.addressIndex.demoui.views
 
-/**
-  * Created by rhysbradbury on 23/01/2017.
-  */
-class SingleMatchHelper {
+import uk.gov.ons.addressIndex.demoui.utils.ClassHierarchy
+import uk.gov.ons.addressIndex.model.server.response.{AddressInformation, NAGWithFormat}
+import play.api.i18n.Messages
 
+object SingleMatchHelper {
+
+  /**
+    * @param address
+    * @return the approved NAG address
+    */
+  def getApprovedNAG(address: AddressInformation): Option[NAGWithFormat] = {
+    address.nag.flatMap(_.find(_.nag.logicalStatus == "1"))
+  }
+
+  /**
+    * @param address
+    * @param classHierarchy
+    * @param messages
+    * @return
+    */
+  def getClassificationCode(
+    address: AddressInformation,
+    classHierarchy: Option[ClassHierarchy]
+  )(implicit messages: Messages): String = {
+    getApprovedNAG(address).flatMap { nag =>
+      classHierarchy map(_ analyseClassCode nag.nag.classificationCode)
+    } getOrElse messages("category.R")
+  }
 }
