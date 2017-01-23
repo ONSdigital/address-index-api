a7d774fe78e55e66d9dbd86ea3e6411a2d3cc7bf
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-23 10:59:18 +0000

    refactoring form to companion

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/model/SingleSearchForm.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/model/SingleSearchForm.scala
@@ -1,17 +1,24 @@
 package uk.gov.ons.addressIndex.demoui.model
 
+import play.api.data.Form
+import play.api.data.Forms.mapping
 import play.api.libs.json._
+import play.api.data.Forms._
 
 /**
   * Form for single address
   */
 
 case class SingleSearchForm(
- address: String,
- format: String = "paf"
+ address: String
 )
 
 object SingleSearchForm {
   val jsonFmt = Json.format[SingleSearchForm]
+  val form = Form(
+    mapping(
+      "address" -> text
+    )(SingleSearchForm.apply)(SingleSearchForm.unapply)
+  )
 }
 
