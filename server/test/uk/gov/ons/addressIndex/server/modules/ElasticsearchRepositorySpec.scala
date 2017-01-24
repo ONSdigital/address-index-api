b7ae8b249a459b7b98b51d2eaa96ff1c4cf4af1b
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-24 11:25:24 +0000

    adding ElasticsearchRepositorySpec coverage for formats

--- server/test/uk/gov/ons/addressIndex/server/modules/ElasticsearchRepositorySpec.scala
+++ server/test/uk/gov/ons/addressIndex/server/modules/ElasticsearchRepositorySpec.scala
@@ -4,7 +4,7 @@ import com.sksamuel.elastic4s.ElasticClient
 import com.sksamuel.elastic4s.testkit.ElasticSugar
 import org.scalatest.{FlatSpec, Matchers}
 import uk.gov.ons.addressIndex.crfscala.CrfScala.CrfTokenResult
-import uk.gov.ons.addressIndex.model.AddressScheme
+import uk.gov.ons.addressIndex.model.{AddressScheme, BritishStandard7666, PostcodeAddressFile}
 import uk.gov.ons.addressIndex.server.model.dao.ElasticClientProvider
 import uk.gov.ons.addressIndex.server.modules.Model.Pagination
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -28,8 +28,16 @@ class ElasticSearchRepositorySpec extends FlatSpec with ElasticSugar with Matche
     }
   }
 
-  it should "produce the correct search definition for a uprn" in {
+  object Formats {
+    val none: Option[AddressScheme] = None
+    val paf: Option[AddressScheme] = Some(PostcodeAddressFile("paf"))
+    val nag: Option[AddressScheme] = Some(BritishStandard7666("bs"))
+  }
+
+
+  it should "produce the correct search definition for a uprn, no format" in {
     val input = "109102"
+    implicit val format: Option[AddressScheme] = Formats.none
     val actual = (repo queryUprnSearchDefinition input).toString.normalise
     val expected = """
                      |{
@@ -51,7 +59,62 @@ class ElasticSearchRepositorySpec extends FlatSpec with ElasticSugar with Matche
     actual shouldBe expected
   }
 
-  it should "produce the correct search definition for an address" in {
+  it should "produce the correct search definition for a uprn, `PostcodeAddressFile` format" in {
+    val input = "109102"
+    implicit val format: Option[AddressScheme] = Formats.paf
+    val actual = (repo queryUprnSearchDefinition input).toString.normalise
+    val expected = """
+                     |{
+                     |    "query" : {
+                     |        "bool" : {
+                     |            "must" : {
+                     |                "match" : {
+                     |                    "uprn" : {
+                     |                        "query" : "109102",
+                     |                        "type" : "boolean"
+                     |                     }
+                     |                 }
+                     |            }
+                     |        }
+                     |    },
+                     |    "_source" : {
+                     |        "includes" : [],
+                     |        "excludes" : ["lpi"]
+                     |    }
+                     | }
+                     |""".stripMargin.normalise
+
+    actual shouldBe expected
+  }
+  it should "produce the correct search definition for a uprn, `BritishStandard7666` format" in {
+    val input = "109102"
+    implicit val format: Option[AddressScheme] = Formats.nag
+    val actual = (repo queryUprnSearchDefinition input).toString.normalise
+    val expected = """
+                     |{
+                     |    "query" : {
+                     |        "bool":{
+                     |            "must":{
+                     |                "match":{
+                     |                    "uprn":{
+                     |                        "query":"109102",
+                     |                        "type":"boolean"
+                     |                     }
+                     |                 }
+                     |            }
+                     |        }
+                     |    },
+                     |    "_source" : {
+                     |        "includes":[],
+                     |        "excludes":["paf"]
+                     |    }
+                     | }
+                     |""".stripMargin.normalise
+
+    actual shouldBe expected
+  }
+
+  it should "produce the correct search definition for an address, no format" in {
     val tokens: Seq[CrfTokenResult] = Seq(
       CrfTokenResult(
         value = "wd244re",
@@ -62,6 +125,7 @@ class ElasticSearchRepositorySpec extends FlatSpec with ElasticSugar with Matche
       offset = 0,
       limit = 10
     )
+    implicit val format: Option[AddressScheme] = Formats.none
     val actual = (repo queryAddressSearchDefinition tokens).toString.normalise
     val expected = """
                      |{
@@ -71,6 +135,70 @@ class ElasticSearchRepositorySpec extends FlatSpec with ElasticSugar with Matche
                      |       "query_string" : {
                      |           "query" : "wd244re"
                      |       }
+                     |   }
+                     |}
+                   """.stripMargin.normalise
+
+    actual shouldBe expected
+  }
+
+  it should "produce the correct search definition for an address, `PostcodeAddressFile` format" in {
+    val tokens: Seq[CrfTokenResult] = Seq(
+      CrfTokenResult(
+        value = "wd244re",
+        label = "Postcode"
+      )
+    )
+    implicit val pagination = Pagination(
+      offset = 0,
+      limit = 10
+    )
+    implicit val format: Option[AddressScheme] = Formats.paf
+    val actual = (repo queryAddressSearchDefinition tokens).toString.normalise
+    val expected = """
+                     |{
+                     |   "from" : 0,
+                     |   "size" : 10,
+                     |   "query" : {
+                     |       "query_string" : {
+                     |           "query" : "wd244re"
+                     |       }
+                     |   },
+                     |   "_source" : {
+                     |       "includes":[],
+                     |       "excludes":["lpi"]
+                     |   }
+                     |}
+                   """.stripMargin.normalise
+
+    actual shouldBe expected
+  }
+
+  it should "produce the correct search definition for an address, `BritishStandard7666` format" in {
+    val tokens: Seq[CrfTokenResult] = Seq(
+      CrfTokenResult(
+        value = "wd244re",
+        label = "Postcode"
+      )
+    )
+    implicit val pagination = Pagination(
+      offset = 0,
+      limit = 10
+    )
+    implicit val format: Option[AddressScheme] = Formats.nag
+    val actual = (repo queryAddressSearchDefinition tokens).toString.normalise
+    val expected = """
+                     |{
+                     |   "from" : 0,
+                     |   "size" : 10,
+                     |   "query" : {
+                     |       "query_string" : {
+                     |           "query" : "wd244re"
+                     |       }
+                     |   },
+                     |   "_source" : {
+                     |       "includes":[],
+                     |       "excludes":["paf"]
                      |   }
                      |}
                    """.stripMargin.normalise
