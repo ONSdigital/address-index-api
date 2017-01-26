63185561d6e928a53b4f9a3cc6b8eee7c750705c
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-01-26 11:41:34 +0000

    refactoring out ElasticDsl to propper place

---
+++ server/app/uk/gov/ons/addressIndex/server/modules/ElasticDsl.scala
@@ -0,0 +1,23 @@
+package uk.gov.ons.addressIndex.demoui.modules
+
+import com.sksamuel.elastic4s.SearchDefinition
+import uk.gov.ons.addressIndex.model.{AddressScheme, BritishStandard7666, PostcodeAddressFile}
+import uk.gov.ons.addressIndex.model.db.index.HybridIndex
+
+object ElasticDsl {
+  case class Pagination(offset: Int, limit: Int)
+  implicit class AutoPaginate(searchDefinition: SearchDefinition) {
+    def paginate(implicit p: Pagination): SearchDefinition = {
+      searchDefinition start p.offset limit p.limit
+    }
+  }
+
+  implicit class AutoSource(searchDefinition: SearchDefinition) {
+    def format(implicit optFmt: Option[AddressScheme]) = {
+      optFmt map {
+        case _: PostcodeAddressFile => searchDefinition sourceExclude HybridIndex.Fields.lpi
+        case _: BritishStandard7666 => searchDefinition sourceExclude HybridIndex.Fields.paf
+      } getOrElse searchDefinition
+    }
+  }
+}
