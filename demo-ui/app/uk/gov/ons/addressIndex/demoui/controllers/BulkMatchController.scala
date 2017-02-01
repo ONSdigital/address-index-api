b7abf850ed1991aa657c63d3e2f4f34bc2a4fade
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-01 15:57:22 +0000

    making navigation reusable

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
@@ -1,15 +1,12 @@
 package uk.gov.ons.addressIndex.demoui.controllers
 
 import javax.inject.{Inject, Singleton}
-
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Controller}
 import uk.gov.ons.addressIndex.demoui.client.AddressIndexClientInstance
 import uk.gov.ons.addressIndex.demoui.modules.DemouiConfigModule
 import uk.gov.ons.addressIndex.demoui.utils.ClassHierarchy
-
 import scala.concurrent.{ExecutionContext, Future}
-
 
 /**
   * Controller class for a multiple addresses to be matched
