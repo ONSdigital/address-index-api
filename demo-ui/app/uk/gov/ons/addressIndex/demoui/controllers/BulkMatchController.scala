d603ad816e150ebfc2605ce16bc7a20f3b9d43dd
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-02 16:10:30 +0000

    bulk-ui working

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
@@ -1,11 +1,18 @@
 package uk.gov.ons.addressIndex.demoui.controllers
 
+import java.util.UUID
 import javax.inject.{Inject, Singleton}
+
+import play.api.Logger
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, AnyContent, Controller}
+import play.api.libs.Files.TemporaryFile
+import play.api.mvc.{Action, AnyContent, Controller, MultipartFormData}
 import uk.gov.ons.addressIndex.demoui.client.AddressIndexClientInstance
+import uk.gov.ons.addressIndex.demoui.model.ui.Navigation
 import uk.gov.ons.addressIndex.demoui.modules.DemouiConfigModule
 import uk.gov.ons.addressIndex.demoui.utils.ClassHierarchy
+import uk.gov.ons.addressIndex.model.AddressIndexSearchRequest
+import scala.io.Source
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
@@ -24,7 +31,43 @@ class BulkMatchController @Inject()(
   classHierarchy: ClassHierarchy
  )(implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
+  private val multiMatchFormName = "file"
+
   def bulkMatchPage(): Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok)
+    Future successful Ok(
+      uk.gov.ons.addressIndex.demoui.views.html.multiMatch(
+        nav = Navigation.default,
+        fileFormName = multiMatchFormName
+      )
+    )
+  }
+
+  def uploadFile(): Action[MultipartFormData[TemporaryFile]] = Action.async(parse.multipartFormData) { implicit request =>
+    request.body.file(multiMatchFormName) map { file =>
+      val seqAddress = Source.fromFile(file.ref.file).mkString.split("\n").toSeq
+      Future.sequence(
+        seqAddress map { address =>
+          apiClient.addressQuery(
+            request = AddressIndexSearchRequest(
+              input = address,
+              limit = "10",
+              offset = "0",
+              id = UUID.randomUUID
+            )
+          )
+        }
+      ) map { resp =>
+        Ok(
+          uk.gov.ons.addressIndex.demoui.views.html.multiMatch(
+            nav = Navigation.default,
+            fileFormName = multiMatchFormName,
+            results = Some(
+              resp
+            )
+          )
+        )
+      }
+
+    } getOrElse Future.successful(InternalServerError)
   }
 }
--- demo-ui/test/uk/gov/ons/addressIndex/demoui/model/ui/NavigationSpec.scala
+++ demo-ui/test/uk/gov/ons/addressIndex/demoui/model/ui/NavigationSpec.scala
@@ -2,9 +2,10 @@ package uk.gov.ons.addressIndex.demoui.model.ui
 
 import org.scalatest.Matchers
 import org.scalatestplus.play.PlaySpec
+import play.api.i18n.{Messages, MessagesApi}
 import play.api.test.WithApplication
 
-class NavigationSpec extends PlaySpec with Matchers {
+class NavigationSpec extends PlaySpec {
 
   object Resources {
     object EN {
@@ -22,24 +23,28 @@ class NavigationSpec extends PlaySpec with Matchers {
       )
     }
   }
+  new WithApplication {
+    implicit val messages: Messages = app.injector.instanceOf[Messages]
 
-  "Naviagtion" should {
-    "have a home link; first" in new WithApplication {
-      val expected = Resources.EN.HomeLinkExpected
-      val actual = Navigation.default.links.head
-      actual shouldBe expected
-    }
+    "Naviagtion" should {
+      "have a home link; first" in {
+        val expected = Resources.EN.HomeLinkExpected
+        val actual = Navigation.default.links.head
+        actual mustBe expected
+      }
 
-    "have a single match link; second" in {
-      val expected = Resources.EN.SingleMatchLinkExpected
-      val actual = Navigation.default.links(1)
-      actual shouldBe expected
-    }
+      "have a single match link; second" in {
+        val expected = Resources.EN.SingleMatchLinkExpected
+        val actual = Navigation.default.links(1)
+        actual mustBe expected
+      }
 
-    "have a multiple match link; third" in {
-      val expected = Resources.EN.MultipleMatchLinkExpected
-      val actual = Navigation.default.links(2)
-      actual shouldBe expected
+      "have a multiple match link; third" in {
+        val expected = Resources.EN.MultipleMatchLinkExpected
+        val actual = Navigation.default.links(2)
+        actual mustBe expected
+      }
     }
   }
+
 }
