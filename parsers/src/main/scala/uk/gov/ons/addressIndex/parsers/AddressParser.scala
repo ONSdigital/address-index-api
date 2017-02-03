202d7892454a440bca7c0c0335d290aedd60bebe
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-03 14:19:26 +0000

    externalising parser resource lib, updating tests also

--- parsers/src/main/scala/uk/gov/ons/addressIndex/parsers/AddressParser.scala
+++ parsers/src/main/scala/uk/gov/ons/addressIndex/parsers/AddressParser.scala
@@ -1,5 +1,6 @@
 package uk.gov.ons.addressIndex.parsers
 
+import com.typesafe.config.ConfigFactory
 import uk.gov.ons.addressIndex.crfscala.CrfAggregateFeatureAnalyser.CrfAggregateFeatureAnalyser
 import uk.gov.ons.addressIndex.crfscala.CrfFeatureAnalyser.CrfFeatureAnalyser
 import uk.gov.ons.addressIndex.crfscala.CrfScala.CrfTokenResult
@@ -10,12 +11,16 @@ import uk.gov.ons.addressIndex.crfscala.{CrfAggregateFeature, CrfFeature, CrfFea
   * AddressParser
   */
 object AddressParser extends CrfParser {
+
+
   def tag(input: String): Seq[CrfTokenResult]  = {
     super.tag(input, FeatureAnalysers.allFeatures, Tokens)
   }
   def parse(input: String): String = {
     super.parse(input, FeatureAnalysers.allFeatures, Tokens)
   }
+
+  override def parserLibPath: String = ConfigFactory.load.getString("addressIndex.parserLibPath")
 }
 
 case class Features(override val features : Feature[_]*)(override val aggregateFeatures: FeatureAggregate[_]*) extends CrfFeatures
--- server/app/uk/gov/ons/addressIndex/server/modules/SystemBootstrap.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/SystemBootstrap.scala
@@ -2,13 +2,15 @@ package uk.gov.ons.addressIndex.server.modules
 
 import java.io.File
 import javax.inject.{Inject, Singleton}
+
+import com.typesafe.config.ConfigFactory
 import play.api.Logger
 
 @Singleton
 class SystemBootstrap @Inject()() {
   System.load(
     new File(
-      "parsers/src/main/resources/libcrftagger.so"
+      s"${ConfigFactory.load().getString("addressIndex.parserLibPath")}/libcrftagger.so"
     ).getAbsolutePath
   )
   Logger("address-index") info "`SystemBootstrap` complete"
