a07be6b1b5be7ba1ff3d762c6ae04c02ea480dce
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-03 12:18:53 +0000

    iterator madness

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/BulkMatchController.scala
@@ -6,14 +6,17 @@ import javax.inject.{Inject, Singleton}
 import play.api.Logger
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.Files.TemporaryFile
-import play.api.mvc.{Action, AnyContent, Controller, MultipartFormData}
+import play.api.mvc._
 import uk.gov.ons.addressIndex.demoui.client.AddressIndexClientInstance
 import uk.gov.ons.addressIndex.demoui.model.ui.Navigation
 import uk.gov.ons.addressIndex.demoui.modules.DemouiConfigModule
 import uk.gov.ons.addressIndex.demoui.utils.ClassHierarchy
 import uk.gov.ons.addressIndex.model.AddressIndexSearchRequest
+import uk.gov.ons.addressIndex.model.server.response.AddressBySearchResponseContainer
+
 import scala.io.Source
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.language.postfixOps
 
 /**
   * Controller class for a multiple addresses to be matched
@@ -29,7 +32,11 @@ class BulkMatchController @Inject()(
   conf: DemouiConfigModule,
   apiClient: AddressIndexClientInstance,
   classHierarchy: ClassHierarchy
- )(implicit ec: ExecutionContext) extends Controller with I18nSupport {
+ )(
+  implicit
+  ec: ExecutionContext,
+  mat: akka.stream.Materializer
+) extends Controller with I18nSupport {
 
   private val multiMatchFormName = "file"
 
@@ -42,35 +49,87 @@ class BulkMatchController @Inject()(
     )
   }
 
-  def uploadFile(): Action[MultipartFormData[TemporaryFile]] = Action.async(parse.multipartFormData) { implicit request =>
-    request.body.file(multiMatchFormName) map { file =>
-      val seqAddress = Source.fromFile(file.ref.file).mkString.split("\n").toSeq
-
-      Logger("TEST").info("SIZE OF ADDRESSES:: " + seqAddress.size.toString)
-
-      Future.sequence(
-        seqAddress map { address =>
-          apiClient.addressQuery(
-            request = AddressIndexSearchRequest(
-              input = address,
-              limit = "10",
-              offset = "0",
-              id = UUID.randomUUID
-            )
-          )
-        }
-      ) map { resp =>
-        Ok(
-          uk.gov.ons.addressIndex.demoui.views.html.multiMatch(
-            nav = Navigation.default,
-            fileFormName = multiMatchFormName,
-            results = Some(
-              seqAddress.zip(resp)
-            )
-          )
+  def q(source: Source): Seq[Future[AddressBySearchResponseContainer]] = {
+    source.getLines.map(line =>
+      apiClient.addressQuery(
+        request = AddressIndexSearchRequest(
+          input = line,
+          limit = "10",
+          offset = "0",
+          id = UUID.randomUUID
         )
-      }
+      )
+    ).toSeq
+  }
 
-    } getOrElse Future.successful(InternalServerError)
+  import scala.concurrent.duration._
+
+  def uploadFile(): Action[Either[MaxSizeExceeded, MultipartFormData[TemporaryFile]]] = Action.async(
+    parse.maxLength(
+      10 * 1024 * 1024, //10MB
+      parse.multipartFormData
+    )
+  ) { implicit request =>
+
+    Logger("uploadFile").info("invoked")
+
+    val optRes = request.body match {
+      case Right(file) => {
+        file.file(multiMatchFormName) map { file =>
+
+          val source = Source.fromFile(file.ref.file)
+          val x = source.iter
+//          val reqs = q(source)
+
+          val test = source.getLines.map(line =>
+            apiClient.addressQuery(
+              request = AddressIndexSearchRequest(
+                input = line,
+                limit = "10",
+                offset = "0",
+                id = UUID.randomUUID
+              )
+            )
+          )
+          println("lines txfd into reqs")
+
+          val s  = test.grouped(30).map(x => Future.sequence(x))
+          println("about to do blocking")
+
+          val y = s.map { f =>
+
+            val z = Await.result(f, 100 seconds)
+            z
+          }
+
+
+
+          pprint.pprintln(y.toSeq)
+
+          Future.successful(Ok)
+//          val seqAddress = source.mkString.split("\n").toSeq
+
+//          Logger("TEST").info("SIZE OF ADDRESSES:: " + seqAddress.size.toString)
+          //
+          //         Future.sequence(reqs) map { resp =>
+//            Logger("szie resp").info(resp.size.toString)
+//            Ok(
+//              uk.gov.ons.addressIndex.demoui.views.html.multiMatch(
+//                nav = Navigation.default,
+//                fileFormName = multiMatchFormName,
+//                results = None
+////                  Some(
+////                  seqAddress.zip(resp)
+////                )
+//              )
+//            )
+//          }
+        }
+      }
+      case Left(maxSizeExceeded) => {
+        Some(Future.successful(EntityTooLarge))
+      }
+    }
+    optRes.getOrElse(Future.successful(InternalServerError))
   }
 }
