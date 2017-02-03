bc0a8fd81a89bc0365a156d7bf56ca33872626b3
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-03 14:40:49 +0000

    adding var lib names

--- server/app/uk/gov/ons/addressIndex/server/modules/SystemBootstrap.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/SystemBootstrap.scala
@@ -8,9 +8,23 @@ import play.api.Logger
 
 @Singleton
 class SystemBootstrap @Inject()() {
+
+  def osSharedObjectName(): String = {
+    val x = System.getProperty("os.name").toLowerCase
+    if(x.contains("mac")) {
+      "libcrftagger.so"
+    } else if(x.contains("window")) {
+      "libcrftagger.dll"
+    } else if(x.contains("linux")) {
+      "libcrftagger-linux.so"
+    } else {
+      "libcrftagger.so"
+    }
+  }
+
   System.load(
     new File(
-      s"${ConfigFactory.load().getString("addressIndex.parserLibPath")}/libcrftagger.so"
+      s"${ConfigFactory.load().getString("addressIndex.parserLibPath")}/$osSharedObjectName"
     ).getAbsolutePath
   )
   Logger("address-index") info "`SystemBootstrap` complete"
