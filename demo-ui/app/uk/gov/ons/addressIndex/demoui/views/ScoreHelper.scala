1672597dcadf1163ef1ed6036235988356214cae
Rhys Bradbury <rhys.alan.bradbury@gmail.com>
2017-02-17 10:37:43 +0000

    filtering by 66%

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/views/ScoreHelper.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/views/ScoreHelper.scala
@@ -1,5 +1,7 @@
 package uk.gov.ons.addressIndex.demoui.views
 
+import uk.gov.ons.addressIndex.model.BulkResp
+
 object ScoreHelper {
   /**
     * calculates score percentage
@@ -10,5 +12,29 @@ object ScoreHelper {
     */
   def getPercentageFromScore(score: Float, maxScore: Float): Int = {
     ((score / maxScore) * 100).toInt
+  }
+
+  /**
+    * calculates relative score percentage
+    *
+    * @param score
+    * @param set
+    * @return
+    */
+  def getRelativePercentageScore(score: Float, set: BulkResp): Int = {
+    ((score / set.resp.maxBy(_.score).score) * 100).toInt
+  }
+
+  /**
+    * filter the results for a BulkResp for items
+    * with a relative score weighing of less than or equal to 66
+    *
+    * @param set
+    * @return
+    */
+  def filterSetByScore(set: BulkResp): BulkResp = {
+    BulkResp(
+      resp = set.resp.filter(i => getRelativePercentageScore(i.score, set) >= 66)
+    )
   }
 }
