2e2b018a2f2ee38d8cc47a7ae712f742a0a6dce5
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2017-04-05 17:06:37 +0100

    Added CORS support.

---
+++ server/app/uk/gov/ons/addressIndex/server/Filters.scala
@@ -0,0 +1,9 @@
+package uk.gov.ons.addressIndex.server
+
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpFilters
+import play.filters.cors.CORSFilter
+
+class Filters @Inject() (corsFilter: CORSFilter)
+  extends DefaultHttpFilters(corsFilter)
