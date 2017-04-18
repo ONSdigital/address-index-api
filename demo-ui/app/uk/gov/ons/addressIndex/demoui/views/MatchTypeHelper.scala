8081745b1facc592541236b0ba1957b0fa26a4f7
Aliaksandr.Bedrytski <aliaksandr.bedrytski@valtech.co.uk>
2017-04-18 15:06:57 +0100

    Adds counters to bulk upload ui

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/views/MatchTypeHelper.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/views/MatchTypeHelper.scala
@@ -1,5 +1,7 @@
 package uk.gov.ons.addressIndex.demoui.views
 
+import uk.gov.ons.addressIndex.model.server.response.AddressBulkResponseAddress
+
 object MatchTypeHelper {
 
   def matchType(addressId: String, ids: Seq[String], matchedFormattedAddress: String): String = {
@@ -12,4 +14,16 @@ object MatchTypeHelper {
       "M"
     }
   }
+
+  def countSearched(addresses: Seq[AddressBulkResponseAddress]): Int = addresses.map(_.id).toSet.size
+
+  def countMultiples(addresses: Seq[AddressBulkResponseAddress]): Int =
+    addresses.filter(_.matchedFormattedAddress.nonEmpty).groupBy(_.id).mapValues(_.size).values.count(_ > 1)
+
+  def countSingles(addresses: Seq[AddressBulkResponseAddress]): Int =
+    addresses.filter(_.matchedFormattedAddress.nonEmpty).groupBy(_.id).mapValues(_.size).values.count(_ == 1)
+
+  def countEmpty(addresses: Seq[AddressBulkResponseAddress]): Int =
+    addresses.count(_.matchedFormattedAddress.isEmpty)
+
 }
