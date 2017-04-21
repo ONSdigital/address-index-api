98e570cf727f973d3943a927074961a015a75ef3
Ibukun O Omidiran <ib@ngwara.com>
2017-04-21 11:58:34 +0100

    Implemented session and Client request Key 🔐
    Authentication keys are stored in user session and the key is used to
    make subsequent requests.

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/ApplicationHomeController.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/controllers/ApplicationHomeController.scala
@@ -11,8 +11,8 @@ import play.api.mvc.{Action, AnyContent, Controller}
 import uk.gov.ons.addressIndex.demoui.model.formdata.LoginCredentials
 import uk.gov.ons.addressIndex.demoui.modules.DemouiConfigModule
 
-import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.language.implicitConversions
 
 /**
@@ -55,47 +55,18 @@ class ApplicationHomeController @Inject()(conf: DemouiConfigModule, val messages
   }
 
   def doLogin: Action[AnyContent] = Action { implicit req =>
-    println("TEST")
+//    println("TEST")
     val formValidationResult = loginForm.bindFromRequest.data
     (for {
       userName <- formValidationResult.get("userName") if userName.nonEmpty
       password <- formValidationResult.get("password") if userName.nonEmpty
     } yield {
-      // there is userName and password values, both are filled
-      println(userName)
-      // Here you have access to userName and password values, if you want, you can do some validation
+//      println(userName)
 
-      /*
-      Now you'll need to send a request to CI gateway
-      https://www.playframework.com/documentation/2.5.x/ScalaWS
-      val complexRequest: WSRequest =
-        request.withHeaders("Accept" -> "application/json")
-        .withRequestTimeout(10000.millis)
-        .withQueryString("search" -> "play")
+      val prod = false // Add to config param
 
-
-      This request will return a Future[WSResponse]
-      val futureResponse: Future[WSResponse] = complexRequest.get()
-
-
-      You can use Await.result(resultOfRequest, 10 seconds) on it as it will be simplier than manipulating Future structure
-
-      val result = Await.result(futureRequest, Duration.INF)
-
-      you will need to process that response (result) in any maner you want
-      https://www.playframework.com/documentation/2.5.x/ScalaWS#Processing-the-Response
-
-      To process it as a JSON you will need to declare a case class for an expdected response and add an implicit formatter to it
-      (for an example check AddressResponse.scala in models project)
-
-
-
-       */
-
-      val prod = false // config param
-
-      val key = if (prod){
-        val request: WSRequest = ws.url("http://ldap_stuff")
+      if (prod) {
+        val request: WSRequest = ws.url("http://localhost:9443/login")
 
         val complexRequest: WSRequest =
           request.withHeaders("Accept" -> "application/json")
@@ -106,70 +77,34 @@ class ApplicationHomeController @Inject()(conf: DemouiConfigModule, val messages
 
         val result = Await.result(futureResponse, 10000.millis)
 
-        (result.json \ "key").as[String]
-      } else ""
+        if (result.status != OK) {
+          val key = (result.json \ "key").as[String]
+
+          Redirect(uk.gov.ons.addressIndex.demoui.controllers.routes.ApplicationHomeController.indexPage())
+            .withSession("api-key" -> key)
+        } else Ok(uk.gov.ons.addressIndex.demoui.views.html.forms.login.fieldset("Authentication failed"))
 
 
-      /*
-      how  to store (and read) a session is written here https://www.playframework.com/documentation/2.5.x/ScalaSessionFlash
+      } else Redirect(uk.gov.ons.addressIndex.demoui.controllers.routes.ApplicationHomeController.indexPage())
+        .withSession("api-key" -> "")
 
-      pay attention that you will need to read this value on every other request here (the list is in routes file)
-      it the value is absent, you should redirect the user to the login page:
-
-      def index = Action { request =>
-        request.session.get("key").map { user =>
-          // DO REQUESTS TO THE API
-       }.getOrElse {
-   // REDIRECT TO LOGIN PAGE
-      }
-      }
-
-
-
-      Now when you are at a request handling method and you have a key, you will need to make a request to the CI gateway the same
-      way you do for the API (bridge API), but now you will also need to add a "key" parameter to it so
-      that the CI gateway accept it
-
-      Currently  (you will find this piece of code in the singlematchcontroller.scala:
-      apiClient.addressQuery(
-        AddressIndexSearchRequest(
-          input = addressText,
-          limit = limit,
-          offset = offset,
-          id = UUID.randomUUID
-        )
-      )
-
-      Modified:
-
-      apiClient.addressQuery(
-        AddressIndexSearchRequest(
-          input = addressText,
-          limit = limit,
-          offset = offset,
-          id = UUID.randomUUID,
-          key = keyYouGotFromSession
-        )
-      )
-
-      DONT FORGET TO MODIFY THE URL THAT IS USED FOR THE REQUEST, currently it's api, it will need to be CI gateway
-
-      The handling of the response of the CI gateway is the same as the actual one, no modification required
-
-       */
-
-      Redirect(uk.gov.ons.addressIndex.demoui.controllers.routes.ApplicationHomeController.indexPage())
-        .withSession("api-key" -> key)
-     // Ok(uk.gov.ons.addressIndex.demoui.views.html.forms.login.fieldset("Empty Username or Password"))
     }).getOrElse {
       // bad, data is not filled or not exist
       Ok(uk.gov.ons.addressIndex.demoui.views.html.forms.login.fieldset("Empty Username or Password"))
     }
-
   }
-
-  //  def list = IsAuthenticated { username => implicit request =>
-  //    val CA = CA.findUSer()
-  //    Ok(uk.gov.ons.addressIndex.demoui.views.html.index())
-
 }
+/* Responses
+
+password <-!CapGateway23
+user <- omidii
+{
+"Status": 401,
+"ErrorCode": "Authentication Error",
+"ErrorMessage": "Authentication failed. Invalid username or password."
+}
+
+{
+"Status": 200,
+"key": "ea15dc73-1991-46db-8c02-e3c483bf9e3e"
+}*/
