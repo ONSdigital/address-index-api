1c7c8be288bde514039c1fdd90cebd1c70892756
Aliaksandr.Bedrytski <aliaksandr.bedrytski@valtech.co.uk>
2017-05-10 10:59:18 +0100

    Loads parser module (with native loadModel method) more lazily to avoid exceptions during build.

--- server/app/uk/gov/ons/addressIndex/server/modules/AddressParserModule.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/AddressParserModule.scala
@@ -21,15 +21,20 @@ trait ParserModule {
 @Singleton
 class AddressParserModule @Inject()(conf: ConfigModule) extends ParserModule {
 
-  private val native = new CrfScalaJniImpl
+  lazy private val native = new CrfScalaJniImpl
 
-  lazy private val parser = new Parser(native)
+  lazy private val parser = loadParser()
 
-  private val currentDirectory = new java.io.File(".").getCanonicalPath
-  private val modelPath = s"$currentDirectory/${conf.config.parserLibPath}/addressCRFA.crfsuite"
+  private def loadParser(): Parser = {
+    val currentDirectory = new java.io.File(".").getCanonicalPath
+    val modelPath = s"$currentDirectory/${conf.config.parserLibPath}/addressCRFA.crfsuite"
 
-  // Singleton annotation will ensure that this is called only once
-  native.loadModel(modelPath)
+    // Singleton annotation will ensure that this is called only once
+    native.loadModel(modelPath)
+
+    new Parser(native)
+  }
+
 
   def parse(input: String): Map[String, String] = parser.parse(input)
 }
