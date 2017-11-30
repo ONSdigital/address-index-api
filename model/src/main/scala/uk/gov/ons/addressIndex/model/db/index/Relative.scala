15323c97e93f1face0e5846ff2a79fba0fa680c2
RichardSmithONS <richard.m.smith@ons.gsi.gov.uk>
2017-11-30 14:28:30 +0000

    deal with Integer uprns

--- model/src/main/scala/uk/gov/ons/addressIndex/model/db/index/Relative.scala
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/db/index/Relative.scala
@@ -34,10 +34,31 @@ object Relative {
     val relIter = rel.asInstanceOf[List[AnyRef]].iterator
     while (relIter.hasNext) {
       val filteredRel = relIter.next().asInstanceOf[Map3[String, AnyRef]].filter { case (_, value) => value != null }
+
+      // uprns sometimes come back as Integers instead of Longs from ES so need to deal with this
+      val relSiblings = filteredRel.getOrElse(Fields.siblings, Seq.empty[Long]).asInstanceOf[List[Any]]
+      val relParents = filteredRel.getOrElse(Fields.siblings, Seq.empty[Long]).asInstanceOf[List[Any]]
+
+      var sibs = Seq.empty[Long]
+
+      for (sib <- relSiblings) {
+        val ssib: String = sib.toString()
+        val lsib: Long = ssib.toLong
+        sibs = sibs :+ lsib
+      }
+
+      var pars = Seq.empty[Long]
+
+      for (par <- relParents) {
+        val spar: String = par.toString()
+        val lpar: Long = spar.toLong
+        pars = pars :+ lpar
+      }
+
       rels = rels :+ Relative(
         level = filteredRel.getOrElse(Fields.level, 0).asInstanceOf[Int],
-        siblings = filteredRel.getOrElse(Fields.siblings, "[]").asInstanceOf[Seq[Long]],
-        parents =  filteredRel.getOrElse(Fields.parents, "[]").asInstanceOf[Seq[Long]]
+        siblings = sibs,
+        parents = pars
       )
     }
     collection.immutable.Seq(rels: _*)
