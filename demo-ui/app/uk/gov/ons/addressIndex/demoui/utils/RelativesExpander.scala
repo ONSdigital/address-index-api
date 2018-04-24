ecffa0ebd038ce586f0c83c1d9c079329269f442
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-04-24 08:30:12 +0100

    Remove dead code from RelativesExpander

--- demo-ui/app/uk/gov/ons/addressIndex/demoui/utils/RelativesExpander.scala
+++ demo-ui/app/uk/gov/ons/addressIndex/demoui/utils/RelativesExpander.scala
@@ -8,8 +8,7 @@ import uk.gov.ons.addressIndex.model.AddressIndexUPRNRequest
 import uk.gov.ons.addressIndex.model.db.index.{ExpandedRelative, ExpandedSibling}
 import uk.gov.ons.addressIndex.model.server.response.{AddressByUprnResponseContainer, AddressResponseRelative}
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 
 @Singleton
@@ -23,46 +22,13 @@ class RelativesExpander @Inject ()(
   private def futExpandRelative(apiKey: String)(rel: AddressResponseRelative): Future[ExpandedRelative] =
     getFutAllExpandedSiblings(apiKey, rel.siblings).map(ExpandedRelative(rel.level, _))
 
-  def expandRelatives(apiKey: String, relatives: Seq[AddressResponseRelative]): Seq[ExpandedRelative] = {
-    relatives.map{rel => expandRelative(apiKey,rel)}
-  }
-
-  private def expandRelative(apiKey: String, rel: AddressResponseRelative): ExpandedRelative = {
-    ExpandedRelative (
-      rel.level,
-      getExpandedSiblings(apiKey, rel.siblings)
-    )
-  }
-
   private def getFutAllExpandedSiblings(apiKey: String, uprns: Seq[Long]): Future[Seq[ExpandedSibling]] =
     Future.sequence(uprns.map(getFutExpandedSibling(apiKey, _)))
-
-  private def getExpandedSiblings(apiKey: String, uprns: Seq[Long]): Seq[ExpandedSibling] = {
-    uprns.map(uprn => {
-     ExpandedSibling(uprn,Await.result(getAddressFromUprn(apiKey,uprn), 1 seconds))
-    })
-  }
 
   private def getFutExpandedSibling(apiKey: String, uprn: Long): Future[ExpandedSibling] =
     getFutAddressByUprn(apiKey, uprn).map {
       (toMixedCase _).andThen(toExpandedSibling(uprn))
     }
-
-  private def getAddressFromUprn(apiKey: String, uprn: Long): Future[String] = {
-    val numericUPRN = BigInt(uprn)
-    apiClient.uprnQuery(
-      AddressIndexUPRNRequest(
-        uprn = numericUPRN,
-        id = UUID.randomUUID,
-        historical = true,
-        apiKey = apiKey
-      )
-    ).map { resp: AddressByUprnResponseContainer =>
-      resp.response.address.map ({ add =>
-        addressToMixedCase(add.formattedAddress)
-      }).getOrElse(uprn + "not found")
-    }
-  }
 
   private def getFutAddressByUprn(apiKey: String, uprn: Long): Future[AddressByUprnResponseContainer] = {
     val numericUPRN = BigInt(uprn)
