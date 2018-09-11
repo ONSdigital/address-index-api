4bfc2e1e9ffdb1bbfd28e60f0206b6da5b9f0b0a
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2018-09-11 15:57:36 +0100

    Create configuration and shell scripts for executing /bulk Gatling tests
    This included additional configuration to support the bulk
    endpoint (for instance the json payload for POST request)

---
+++ server/src/it/scala/uk/gov/ons/gatling/simulations/RegistersSimulationClosedModel.scala
@@ -0,0 +1,50 @@
+package uk.gov.ons.gatling.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+import uk.gov.ons.gatling.conf.ConfigLoader
+
+import scala.language.postfixOps
+
+class RegistersSimulationClosedModel extends Simulation {
+
+  private val baseUrl: String = ConfigLoader("baseUrl")
+  private val numOfRequestsPerSecond: Int = ConfigLoader("requestsPerSecond") toInt
+  private val requestRelPath = ConfigLoader("request_rel_path")
+  private val requestType = ConfigLoader("request_type")
+  private val concurrentUsers = Integer.parseInt(ConfigLoader("concurrent_users"))
+  private val requestName: String = ConfigLoader("request_name_prefix").stripSuffix(" ") + ": " + baseUrl + requestRelPath
+
+  println(s"Running test with numOfRequestsPerSecond: $numOfRequestsPerSecond, " +
+    s"concurrentUsers : $concurrentUsers, " +
+    s"baseUrl : $baseUrl, $requestType Request : $requestRelPath")
+
+  private val httpProtocol: HttpProtocolBuilder = http
+    .baseURL(baseUrl)
+    .inferHtmlResources()
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-GB,en;q=0.5")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0")
+    .shareConnections
+
+  private val postRequestBody = ConfigLoader.getPOSTRequestBodyJSONPath()
+  println(s"payload name $postRequestBody")
+
+  val headers = Map("Upgrade-Insecure-Requests" -> "1")
+  private val httpRequestBuilder = if (requestType == "POST") {
+    val postRequestBody = ConfigLoader.getPOSTRequestBodyJSONPath()
+    http(requestName)
+      .post(requestRelPath)
+      .headers(headers)
+      .body(RawFileBody(postRequestBody)).asJSON
+  } else {
+    http(requestName)
+      .get(requestRelPath)
+      .headers(headers)
+  }
+
+  setUp(scenario("All").exec(httpRequestBuilder).inject(atOnceUsers(concurrentUsers))
+    .protocols(httpProtocol))
+}
