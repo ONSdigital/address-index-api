0071939e33d0a05002e2d49321a6e390417d5a66
RichardSmithONS <richard.m.smith@ons.gsi.gov.uk>
2018-10-01 17:02:00 +0100

    fix tokens, expand verbose description, add errors

---
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressBySearchResponseConcise.scala
@@ -0,0 +1,36 @@
+package uk.gov.ons.addressIndex.model.server.response.address
+
+import play.api.libs.json.{Format, Json}
+
+/**
+  * Contains relevant, to the address request, data
+  *
+  * @param tokens    address decomposed into relevant parts (building number, city, street, etc.)
+  * @param addresses found addresses
+  * @param limit     max number of found addresses
+  * @param offset    offset of found addresses (for pagination)
+  * @param total     total number of found addresses
+  */
+case class AddressBySearchResponseConcise(
+  tokens: Map[String, String],
+  addresses: Seq[AddressResponseAddressConcise],
+  filter: String,
+  historical: Boolean,
+  rangekm: String,
+  latitude: String,
+  longitude: String,
+  startDate: String,
+  endDate: String,
+  limit: Int,
+  offset: Int,
+  total: Long,
+  sampleSize: Long,
+  maxScore: Double,
+  matchthreshold: Float,
+  verbose: Boolean
+)
+
+object AddressBySearchResponseConcise {
+  implicit lazy val addressBySearchResponseConciseFormat: Format[AddressBySearchResponseConcise] = Json.format[AddressBySearchResponseConcise]
+}
+
---
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressResponseAddressConcise.scala
@@ -0,0 +1,77 @@
+package uk.gov.ons.addressIndex.model.server.response.address
+
+import play.api.libs.json.{Format, Json}
+import uk.gov.ons.addressIndex.model.db.index.{HybridAddress, NationalAddressGazetteerAddress, PostcodeAddressFileAddress}
+
+/**
+  * Contains address information retrieved in ES (PAF or NAG)
+  *
+  * @param uprn               uprn
+  * @param formattedAddress   cannonical address form
+  * @param paf                optional, information from Paf index
+  * @param nag                optional, information from Nag index
+  * @param underlyingScore    score from elastic search
+  *
+  */
+case class AddressResponseAddressConcise(
+  uprn: String,
+  parentUprn: String,
+  formattedAddress: String,
+  formattedAddressNag: String,
+  formattedAddressPaf: String,
+  welshFormattedAddressNag: String,
+  welshFormattedAddressPaf: String,
+  confidenceScore: Double,
+  underlyingScore: Float
+)
+
+object AddressResponseAddressConcise {
+  implicit lazy val addressResponseAddressConciseFormat: Format[AddressResponseAddressConcise] = Json.format[AddressResponseAddressConcise]
+
+  /**
+    * Transforms hybrid object returned by ES into an Address that will be in the json response
+    * @param other HybridAddress from ES
+    * @return
+    */
+  def fromHybridAddress(other: HybridAddress, verbose: Boolean): AddressResponseAddressConcise = {
+
+    val chosenNag: Option[NationalAddressGazetteerAddress] = chooseMostRecentNag(other.lpi, NationalAddressGazetteerAddress.Languages.english)
+    val formattedAddressNag = if (chosenNag.isEmpty) "" else chosenNag.get.mixedNag
+
+    val chosenWelshNag: Option[NationalAddressGazetteerAddress] = chooseMostRecentNag(other.lpi, NationalAddressGazetteerAddress.Languages.welsh)
+    val welshFormattedAddressNag = if (chosenWelshNag.isEmpty) "" else chosenWelshNag.get.mixedNag
+
+    val chosenPaf: Option[PostcodeAddressFileAddress] =  other.paf.headOption
+    val formattedAddressPaf = if (chosenPaf.isEmpty) "" else chosenPaf.get.mixedPaf
+    val welshFormattedAddressPaf = if (chosenPaf.isEmpty) "" else chosenPaf.get.mixedWelshPaf
+
+    AddressResponseAddressConcise(
+      uprn = other.uprn,
+      parentUprn = other.parentUprn,
+      formattedAddress = formattedAddressNag,
+      formattedAddressNag = formattedAddressNag,
+      formattedAddressPaf = formattedAddressPaf,
+      welshFormattedAddressNag = welshFormattedAddressNag,
+      welshFormattedAddressPaf = welshFormattedAddressPaf,
+      confidenceScore = 1D,
+      underlyingScore = other.score
+    )
+  }
+
+  /**
+    * Gets the right (most often - the most recent) address from an array of NAG addresses
+    * @param addresses list of Nag addresses
+    * @return the NAG address that corresponds to the returned address
+    */
+  def chooseMostRecentNag(addresses: Seq[NationalAddressGazetteerAddress], language: String): Option[NationalAddressGazetteerAddress] ={
+    // "if" is more readable than "getOrElse" in this case
+    if (addresses.exists(address => address.lpiLogicalStatus == "1" && address.language == language ))
+      addresses.find(_.lpiLogicalStatus == "1")
+    else if (addresses.exists(address => address.lpiLogicalStatus == "6" && address.language == language))
+      addresses.find(_.lpiLogicalStatus == "6")
+    else if (addresses.exists(address => address.lpiLogicalStatus == "8" && address.language == language))
+      addresses.find(_.lpiLogicalStatus == "8")
+    else addresses.headOption
+  }
+}
+
