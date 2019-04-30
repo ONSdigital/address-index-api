a4579f9871dcb94e1b149ce7ca02e2ad60853478
Richard Smith <richard.m.smith@ons.gov.uk>
2019-04-30 14:34:44 +0100

    add no app hierarchy test

---
+++ demo-ui/test/uk/gov/ons/addressIndex/demoui/utils/ClassHierarchy_NoApplicationTest.scala
@@ -0,0 +1,107 @@
+package uk.gov.ons.addressIndex.demoui.utils
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+
+class ClassHierarchy_NoApplicationTest extends FlatSpec with Matchers with MockFactory with ScalaFutures {
+
+  private trait Fixture {
+
+    
+  }
+
+//  class ClassHierarchyTest extends FlatSpec with Matchers {
+//    "ClassHierarchy" should
+//      "return the original code and the single classification" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ C ] [ Commercial ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("C")
+//
+//      result shouldBe expectedSeq
+//    }
+//
+//    "ClassHierarchy" should
+//      "return the original code and the two classifications in the hierarchy" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ CL ] [ Commercial ] [ Leisure - Applicable to recreational sites and enterprises ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("CL")
+//
+//      result shouldBe expectedSeq
+//    }
+//
+//    "ClassHierarchy" should
+//      "return the original code and the three classifications in the hierarchy" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ CL06 ] [ Commercial ] [ Leisure - Applicable to recreational sites and enterprises ] [ Indoor / Outdoor Leisure / Sporting Activity / Centre ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("CL06")
+//
+//      result shouldBe expectedSeq
+//    }
+//
+//    "ClassHierarchy" should
+//      "return the original code and the four classifications in the hierarchy" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ CL06RG ] [ Commercial ] [ Leisure - Applicable to recreational sites and enterprises ] [ Indoor / Outdoor Leisure / Sporting Activity / Centre ] [ Recreation Ground ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("CL06RG")
+//
+//      result shouldBe expectedSeq
+//    }
+//
+//    "ClassHierarchy" should
+//      "return the original unknown code" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ AB12AB ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("AB12AB")
+//
+//      result shouldBe expectedSeq
+//    }
+//
+//    "ClassHierarchy" should
+//      "return the original code and the three classifications in the (partial) hierarchy" in new WithApplication {
+//      val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+//      val langs: Langs = app.injector.instanceOf[Langs]
+//      val classHierarchy = new ClassHierarchy(messagesApi, langs)
+//
+//      // Given
+//      val expectedSeq = " [ MF99UG ] [ Military ] [ Air Force ] [ Air Force Military Storage ]"
+//
+//      // When
+//      val result: String = classHierarchy.analyseClassCode("MF99UG")
+//
+//      result shouldBe expectedSeq
+//    }
+//  }
+
+
+}
