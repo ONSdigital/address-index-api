ea5fff0a02b39b7aa18a253b1ab62556c23d8256
Richard Smith <richard.m.smith@ons.gov.uk>
2019-05-13 14:56:49 +0100

    single match completed

--- demo-ui/test/uk/gov/ons/addressIndex/demoui/controllers/SingleMatchNoAppTest.scala
+++ demo-ui/test/uk/gov/ons/addressIndex/demoui/controllers/SingleMatchNoAppTest.scala
@@ -2,15 +2,15 @@ package uk.gov.ons.addressIndex.demoui.controllers
 
 import org.scalatestplus.play.PlaySpec
 import org.specs2.execute.Results
-import play.api.Environment
 import play.api.i18n.{DefaultLangs, DefaultMessagesApi, Langs}
 import play.api.libs.ws.WSClient
 import play.api.test.{FakeRequest, WsTestClient}
 import play.api.mvc.Result
-import play.api.test.Helpers.{contentAsString, status, defaultAwaitTimeout}
+import play.api.test.Helpers.{POST, contentAsString, defaultAwaitTimeout, status}
 import uk.gov.ons.addressIndex.demoui.client.AddressIndexClientMock
-import uk.gov.ons.addressIndex.demoui.modules.{DemoUIAddressIndexVersionModule, DemoUIVersionModuleMock, DemouiConfigModule, DemouiConfigModuleMock}
-import uk.gov.ons.addressIndex.demoui.utils.{ClassHierarchy, RelativesExpander,StubFactory}
+import uk.gov.ons.addressIndex.demoui.modules.{DemoUIVersionModuleMock, DemouiConfigModuleMock}
+import uk.gov.ons.addressIndex.demoui.utils.{ClassHierarchy, RelativesExpander, StubFactory}
+
 import scala.concurrent.{ExecutionContext, Future}
 
 class SingleMatchNoAppTest extends PlaySpec with Results {
@@ -18,7 +18,7 @@ class SingleMatchNoAppTest extends PlaySpec with Results {
   private trait Fixture {
 
     implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-    val conf: DemouiConfigModule = new DemouiConfigModuleMock
+    val conf: DemouiConfigModuleMock = new DemouiConfigModuleMock
     val wsClient =  WsTestClient.withClient[WSClient](identity)
     val addressIndexClientMock: AddressIndexClientMock = new AddressIndexClientMock(wsClient, conf)
     val version = new DemoUIVersionModuleMock(addressIndexClientMock, executionContext)
@@ -31,6 +31,11 @@ class SingleMatchNoAppTest extends PlaySpec with Results {
         "category.M" -> "Military",
         "category.MF" -> "Air Force",
         "category.MF99UG" -> "Air Force Military Storage",
+        "category.R" -> "Residential",
+        "category.RD" -> "Dwelling",
+        "results.foundexactpre" -> "We have matched",
+        "results.foundpost" -> "addresses",
+        "single.pleasesupply" -> "Please enter an address",
         "single.sfatext" -> "Search for an address"))
     )
     val langs: Langs = new DefaultLangs()
@@ -52,7 +57,7 @@ class SingleMatchNoAppTest extends PlaySpec with Results {
   }
 
   "Single Match Controller" should {
-    "include at least one link" in new Fixture {
+    "return the initial search page" in new Fixture {
 
       // Given
       val expectedString = "Search for an address"
@@ -65,7 +70,59 @@ class SingleMatchNoAppTest extends PlaySpec with Results {
 
       // Then
       status(response) mustBe 200
-     // println(content)
+      content must include(expectedString)
+    }
+
+    "return a page including an appropriate error message when empty address posted" in new Fixture {
+
+      // Given
+      val expectedString = "<div class=\"warning-error-suggestion mars\" role=\"alert\"><span onclick=\"setFocus('address');\">Please enter an address</span></div>"
+
+      // When
+      val response: Future[Result] = singleController.
+        doMatch().apply(FakeRequest(POST, "/addresses/search").withFormUrlEncodedBody("address" -> "").withSession("api-key" -> ""))
+
+
+      val content = contentAsString(response)
+
+      // Then
+      status(response) mustBe 200
+      content must include(expectedString)
+    }
+
+    "return a page including some search results" in new Fixture {
+
+      // Given
+      val expectedString = "<div class=\"standout\">We have matched 1 addresses</div>"
+      val inputAddress = "7 EX2 6GA"
+      val filter=""
+
+      // When
+      val response: Future[Result] = singleController.
+        doMatchWithInput(inputAddress, Some(filter), Some(1)).apply(FakeRequest().withSession("api-key" -> ""))
+
+      val content = contentAsString(response)
+
+      // Then
+      status(response) mustBe 200
+      content must include(expectedString)
+    }
+
+    "return a page including some search results with a filter" in new Fixture {
+
+      // Given
+      val expectedString = "[ RD ] [ Residential ] [ Dwelling ]"
+      val inputAddress = "7 EX2 6GA"
+      val filter = "RD"
+
+      // When
+      val response: Future[Result] = singleController.
+        doMatchWithInput(inputAddress, Some(filter), Some(1)).apply(FakeRequest().withSession("api-key" -> ""))
+
+      val content = contentAsString(response)
+
+      // Then
+      status(response) mustBe 200
       content must include(expectedString)
     }
   }
