f980ea566468636e3336e47a8af34c1dfb029197
Richard Smith <richard.m.smith@ons.gov.uk>
2019-08-02 16:41:10 +0100

    remove logging of query

--- server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexRepository.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexRepository.scala
@@ -3,7 +3,6 @@ package uk.gov.ons.addressIndex.server.modules
 import com.sksamuel.elastic4s.analyzers.CustomAnalyzer
 import com.sksamuel.elastic4s.http.ElasticDsl.{geoDistanceQuery, _}
 import com.sksamuel.elastic4s.http.HttpClient
-import com.sksamuel.elastic4s.http.search.SearchBodyBuilderFn
 import com.sksamuel.elastic4s.searches.queries.{BoolQueryDefinition, ConstantScoreDefinition, QueryDefinition}
 import com.sksamuel.elastic4s.searches.sort.{FieldSortDefinition, SortOrder}
 import com.sksamuel.elastic4s.searches.{SearchDefinition, SearchType}
@@ -868,7 +867,6 @@ class AddressIndexRepository @Inject()(conf: AddressIndexConfigModule,
 
   override def runMultiResultQuery(args: MultiResultArgs): Future[HybridAddressCollection] = {
     val query = makeQuery(args)
-    println(SearchBodyBuilderFn(query).string())
     args match {
       case partialArgs: PartialArgs =>
         val minimumFallback: Int = esConf.minimumFallback
