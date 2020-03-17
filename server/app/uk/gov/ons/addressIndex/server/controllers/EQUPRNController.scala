01cef4fdf02ebd5e742beb499611a79edf7762a2
Richard Smith <richard.m.smith@ons.gov.uk>
2020-03-17 11:12:38 +0000

    revert addresstype to mandatory

--- server/app/uk/gov/ons/addressIndex/server/controllers/EQUPRNController.scala
+++ server/app/uk/gov/ons/addressIndex/server/controllers/EQUPRNController.scala
@@ -23,7 +23,7 @@ class EQUPRNController @Inject()(val controllerComponents: ControllerComponents,
                                  versionProvider: VersionModule,
                                  overloadProtection: APIThrottle,
                                  uprnValidation: UPRNControllerValidation
-                              )(implicit ec: ExecutionContext)
+                                )(implicit ec: ExecutionContext)
   extends PlayHelperController(versionProvider) with UPRNControllerResponse {
 
   lazy val logger = new AddressAPILogger("address-index-server:UPRNController")
@@ -35,7 +35,7 @@ class EQUPRNController @Inject()(val controllerComponents: ControllerComponents,
     * @return
     */
   def uprnQuery(uprn: String,
-                bestMatchAddressType: Option[String] = None,
+                bestMatchAddressType: String,
                 historical: Option[String] = None,
                 verbose: Option[String] = None,
                 epoch: Option[String] = None
@@ -47,15 +47,14 @@ class EQUPRNController @Inject()(val controllerComponents: ControllerComponents,
 
     val hist = historical.flatMap(x => Try(x.toBoolean).toOption).getOrElse(true)
     val verb = verbose.flatMap(x => Try(x.toBoolean).toOption).getOrElse(false)
-    val bestMatchAddressTypeVal = bestMatchAddressType.getOrElse("paf")
 
-    val addressType:String =  bestMatchAddressTypeVal match {
+    val addressType = bestMatchAddressType match {
       case "paf" => AddressResponseAddress.AddressTypes.paf
       case "welshpaf" => AddressResponseAddress.AddressTypes.welshPaf
       case "nag" => AddressResponseAddress.AddressTypes.nag
       case "welshnag" => AddressResponseAddress.AddressTypes.welshNag
       case "nisra" => AddressResponseAddress.AddressTypes.nisra
-      case _ => bestMatchAddressTypeVal
+      case _ => bestMatchAddressType
     }
 
     val epochVal = epoch.getOrElse("")
@@ -78,7 +77,7 @@ class EQUPRNController @Inject()(val controllerComponents: ControllerComponents,
 
     val queryValues = QueryValues(
       uprn = Some(uprn),
-      addressType = Some(bestMatchAddressTypeVal),
+      addressType = Some(bestMatchAddressType),
       epoch = Some(epochVal),
       historical = Some(hist),
       verbose = Some(verb),
@@ -86,7 +85,7 @@ class EQUPRNController @Inject()(val controllerComponents: ControllerComponents,
 
     val result: Option[Future[Result]] =
       uprnValidation.validateUprn(uprn, queryValues)
-        .orElse(uprnValidation.validateAddressType(bestMatchAddressTypeVal, queryValues))
+        .orElse(uprnValidation.validateAddressType(bestMatchAddressType, queryValues))
         .orElse(uprnValidation.validateSource(queryValues))
         .orElse(uprnValidation.validateKeyStatus(queryValues))
         .orElse(uprnValidation.validateEpoch(queryValues))
