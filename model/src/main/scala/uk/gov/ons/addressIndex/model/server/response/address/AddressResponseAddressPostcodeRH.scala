082c5b6fbf15f4d43fa5e11471963a912115e9fb
Richard Smith <richard.m.smith@ons.gov.uk>
2020-06-26 14:32:38 +0100

    correct logic in AddressResponsePostcodeRH and EQ for NISRA

--- model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressResponseAddressPostcodeRH.scala
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressResponseAddressPostcodeRH.scala
@@ -50,13 +50,16 @@ object AddressResponseAddressPostcodeRH {
       if (favourPaf) {
         if (favourWelsh) {
           if (welshFormattedAddressPaf.isEmpty) {
-            if (welshFormattedAddressNag.isEmpty) (formattedAddressNag, AddressTypes.nag) else (welshFormattedAddressNag, AddressTypes.welshNag)
-          } else {
+            if (welshFormattedAddressNag.isEmpty) {
+              if (chosenNisra.isEmpty) (formattedAddressNag, AddressTypes.nag) else (formattedAddressNisra, AddressTypes.nisra)
+              }
+              else (welshFormattedAddressNag, AddressTypes.welshNag)
+           } else {
             (welshFormattedAddressPaf, AddressTypes.welshPaf)
           }
         } else {
           if (formattedAddressPaf.isEmpty) {
-            (formattedAddressNag, AddressTypes.nag)
+            if (chosenNisra.isEmpty) (formattedAddressNag, AddressTypes.nag) else (formattedAddressNisra, AddressTypes.nisra)
           } else {
             (formattedAddressPaf, AddressTypes.paf)
           }
