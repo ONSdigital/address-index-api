872f4eff0404157afdc1ade6b06cf512231f94b1
Richard Smith <richard.m.smith@ons.gov.uk>
2021-01-08 14:56:42 +0000

    add new combined highlight routines

--- model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressResponseHighlightHit.scala
+++ model/src/main/scala/uk/gov/ons/addressIndex/model/server/response/address/AddressResponseHighlightHit.scala
@@ -21,4 +21,16 @@ object AddressResponseHighlightHit {
 
    Some(AddressResponseHighlightHit(source,lang,distinctHitCount,highLightedText))
   }
+
+  def fromCombinedHighlight(hit: (String,Seq[String]), overrideText:String = "", testFieldName:String, testFieldVal:String): Option[AddressResponseHighlightHit] = {
+
+    val highlightedTextIn = if (overrideText != "") overrideText else hit._2.mkString
+    val highlightedTextOut = testFieldVal
+    val searchField = testFieldName
+    val lang = if (searchField.contains("welsh")) "W" else "E"
+    val source = if (searchField.contains("Nag")) "L" else if (searchField.contains("Nisra")) "N" else "P"
+    val distinctHitCount = Math.round(highlightedTextOut.mkString.replace(",","").split(" ").distinct.mkString.count(_ == '<') / 2)
+
+    Some(AddressResponseHighlightHit(source,lang,distinctHitCount,highlightedTextOut))
+  }
 }
