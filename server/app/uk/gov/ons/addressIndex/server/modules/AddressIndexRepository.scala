99aeabb8e6bd82a75f374f65017bd073d0a100ed
Richard Smith <richard.m.smith@ons.gov.uk>
2023-05-11 14:16:31 +0100

    try postal query without boost - might be cached

--- server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexRepository.scala
+++ server/app/uk/gov/ons/addressIndex/server/modules/AddressIndexRepository.scala
@@ -415,7 +415,7 @@ class AddressIndexRepository @Inject()(conf: ConfigModule,
         constantScoreQuery(termsQuery(
           field = "lpi.addressBasePostal",
           values = Seq("D","C","L")
-        )).boost(queryParams.postal.postalDBoost)
+        ))
       )
 
     // this part of query should be blank unless there is an end number or end suffix
@@ -1001,8 +1001,8 @@ class AddressIndexRepository @Inject()(conf: ConfigModule,
       organisationDepartmentQueries,
       townLocalityQueries,
       paoQuery,
-      saoQuery
-  //    postalQuery
+      saoQuery,
+      postalQuery
       // `dismax` dsl does not exist, `: _*` means that we provide a list (`queries`) as arguments (args) for the function
     ).filter(_.nonEmpty).map(queries => dismax(queries: Iterable[Query]).tieBreaker(queryParams.includingDisMaxTieBreaker))
 
